3338187e3791d6e844028d91b649576a940f8bf9
Moamal-2000 <moamalalaapro1@gmail.com>
2025-02-06 23:18:01 +0300

    remove: the whole project, & convert the project to nextjs

--- frontend/src/Routes/routes.jsx
+++
@@ -1,7 +0,0 @@
-import Home from "../Components/Home/Home";
-import PageNotFound from "../Components/PageNotFound/PageNotFound";
-
-export const routes = [
-  { path: "/", element: <Home /> },
-  { path: "*", element: <PageNotFound /> },
-];
