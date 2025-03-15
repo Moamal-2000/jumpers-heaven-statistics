afead75ce51d351bf8b50047aa8f624b971ee1c6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-15 19:13:13 +0300

    init: Header content

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import SvgIcon from "../Shared/SvgIcon";
 import s from "./Header.module.scss";
 
 const Header = () => {
-  return <header className={s.header}></header>;
+  return (
+    <header className={s.header}>
+      <Link href="/" className={s.logo}>
+        <SvgIcon name="trophy" />
+        <h1>JumpersHeaven</h1>
+      </Link>
+
+      {/* <MainNav /> */}
+    </header>
+  );
 };
 
 export default Header;
