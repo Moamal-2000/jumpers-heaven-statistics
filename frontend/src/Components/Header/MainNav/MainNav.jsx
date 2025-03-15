c4c7ece3741f1ad6db1f43419168b40ae493684f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-15 19:35:07 +0300

    display: iterate on nav links to display them in the header

--- frontend/src/Components/Header/MainNav/MainNav.jsx
+++ frontend/src/Components/Header/MainNav/MainNav.jsx
@@ -1,7 +1,17 @@
+import { NAV_LINKS_DATA } from "@/Data/staticData";
+import Link from "next/link";
 import s from "./MainNav.module.scss";
 
 const MainNav = () => {
-  return <nav className={s.mainNav}></nav>;
+  return (
+    <nav className={s.mainNav}>
+      {NAV_LINKS_DATA.map(({ name, href, id }) => (
+        <Link key={id} href={href}>
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default MainNav;
---
+++ frontend/src/Data/staticData.js
@@ -0,0 +1,22 @@
+export const NAV_LINKS_DATA = [
+  {
+    name: "Home",
+    href: "/",
+    id: 1,
+  },
+  {
+    name: "Maps",
+    href: "/maps",
+    id: 2,
+  },
+  {
+    name: "Player",
+    href: "/players",
+    id: 3,
+  },
+  {
+    name: "About",
+    href: "/about",
+    id: 4,
+  },
+];
