c934c42a7d80d9e9e636b3c424d8564c87ef3e01
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-15 19:54:14 +0300

    style: MainNav component

--- frontend/src/Components/Header/MainNav/MainNav.jsx
+++ frontend/src/Components/Header/MainNav/MainNav.jsx
@@ -1,3 +1,4 @@
+import SvgIcon from "@/Components/Shared/SvgIcon";
 import { NAV_LINKS_DATA } from "@/Data/staticData";
 import Link from "next/link";
 import s from "./MainNav.module.scss";
@@ -5,9 +6,10 @@ import s from "./MainNav.module.scss";
 const MainNav = () => {
   return (
     <nav className={s.mainNav}>
-      {NAV_LINKS_DATA.map(({ name, href, id }) => (
+      {NAV_LINKS_DATA.map(({ name, href, iconName, id }) => (
         <Link key={id} href={href}>
-          {name}
+          <SvgIcon name={iconName} />
+          <span>{name}</span>
         </Link>
       ))}
     </nav>
--- frontend/src/Data/staticData.js
+++ frontend/src/Data/staticData.js
@@ -2,21 +2,25 @@ export const NAV_LINKS_DATA = [
   {
     name: "Home",
     href: "/",
+    iconName: "home",
     id: 1,
   },
   {
     name: "Maps",
     href: "/maps",
+    iconName: "map",
     id: 2,
   },
   {
     name: "Player",
     href: "/players",
+    iconName: "users",
     id: 3,
   },
   {
     name: "About",
     href: "/about",
+    iconName: "exclamation-mark",
     id: 4,
   },
 ];
