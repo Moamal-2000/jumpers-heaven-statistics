35aa18e81d169a2c9283118fa68dcdfd158ea681
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-15 19:57:11 +0300

    style: highlight active nav link depending on page route

--- frontend/src/Components/Header/MainNav/MainNav.jsx
+++ frontend/src/Components/Header/MainNav/MainNav.jsx
@@ -1,13 +1,22 @@
+"use client";
+
 import SvgIcon from "@/Components/Shared/SvgIcon";
 import { NAV_LINKS_DATA } from "@/Data/staticData";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import s from "./MainNav.module.scss";
 
 const MainNav = () => {
+  const currentPage = usePathname();
+
   return (
     <nav className={s.mainNav}>
       {NAV_LINKS_DATA.map(({ name, href, iconName, id }) => (
-        <Link key={id} href={href}>
+        <Link
+          key={id}
+          href={href}
+          className={currentPage === href ? s.active : ""}
+        >
           <SvgIcon name={iconName} />
           <span>{name}</span>
         </Link>
