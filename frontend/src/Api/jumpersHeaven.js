7dabe661650cd1c5b437236d74a74ac16ce67920
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-15 14:05:25 +0300

    write: function to get jh apis

---
+++ frontend/src/Api/jumpersHeaven.js
@@ -0,0 +1,21 @@
+export const jhApis = ({
+  fps = 125,
+  limit = 50,
+  mapId = 173824,
+  ender = null,
+  name = "SAD",
+  playerId = 108468,
+  cpId = 14606,
+}) => {
+  const domain = "https://jhstats.fly.dev/api/v1";
+
+  return {
+    mostFinished: `${domain}/map/getMostFinished?fps=${fps}&limit=${limit}`,
+    mapPlayersPlayTime: `${domain}/map/getPlayersPlaytime?fps=${fps}&limit=${limit}&mapid=${mapId}&ender=${ender}`,
+    allMaps: `${domain}/map/getAll`,
+    playerIdByName: `${domain}/player/getIdFromName?name=${name}&limit=${limit}`,
+    playerMapFinishCount: `${domain}/player/getMapFinishCount?playerid=${playerId}&cpid=${cpId}&fps=${fps}`,
+    globalLeaderboard: `${domain}/leaderboard/getGlobalLeaderboard?fps=${fps}`,
+    playerTops: `${domain}/leaderboard/getPlayerTops?fps=${fps}&playerid=${playerId}`,
+  };
+};
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,11 @@
+import { jhApis } from "@/Api/jumpersHeaven";
 import s from "./page.module.scss";
 
-export default function Home() {
+export default async function Home() {
+  const testRequest = await fetch(jhApis({ fps: 125, limit: 100 }).allMaps);
+  const data = testRequest.json();
+
+  console.log(await data);
+
   return <main className={s.home}></main>;
 }
