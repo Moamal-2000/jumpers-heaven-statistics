613f16de60d2a61acb5d25e91176095a2e2cd97a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-16 21:47:20 +0300

    feat: add additional players to the global leaderboard

--- frontend/src/Data/staticData.js
+++ frontend/src/Data/staticData.js
@@ -30,4 +30,10 @@ export const GLOBAL_LEADERBOARD = [
   { rank: 2, player: "JumpMaster", score: 2123, tops: 35, id: 2 },
   { rank: 3, player: "Rocket", score: 1989, tops: 31, id: 3 },
   { rank: 4, player: "BunnyHop", score: 1876, tops: 28, id: 4 },
+  { rank: 5, player: "BunnyHop", score: 1654, tops: 25, id: 5 },
+  { rank: 6, player: "AirTime", score: 1543, tops: 22, id: 6 },
+  { rank: 7, player: "Speedster", score: 1432, tops: 19, id: 7 },
+  { rank: 8, player: "FlightRisk", score: 1321, tops: 17, id: 8 },
+  { rank: 9, player: "Dasher", score: 1234, tops: 15, id: 9 },
+  { rank: 10, player: "ZoomZoom", score: 1123, tops: 12, id: 10 },
 ];
