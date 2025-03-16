e9fbf3ad4e57bff6cf6dbf71def65c2d319ca655
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-16 20:28:32 +0300

    write: LeaderBoard structure/dummy data

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
@@ -1,9 +1,28 @@
-import s from './LeaderBoard.module.scss'
+import { GLOBAL_LEADERBOARD } from "@/Data/staticData";
+import s from "./LeaderBoard.module.scss";
 
 const LeaderBoard = () => {
   return (
-    <table className={s.leaderBoard}></table>
-  )
-}
+    <table className={s.leaderBoard}>
+      <thead>
+        <th className={s.rank}>Rank</th>
+        <th className={s.player}>Player</th>
+        <th className={s.score}>Score</th>
+        <th className={s.tops}>Tops 1-10</th>
+      </thead>
 
-export default LeaderBoard
+      <tbody>
+        {GLOBAL_LEADERBOARD.map(({ rank, player, score, tops, id }) => (
+          <tr key={id}>
+            <td className={s.rank}>{rank}</td>
+            <td className={s.player}>{player}</td>
+            <td className={s.score}>{score}</td>
+            <td className={s.tops}>{tops}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default LeaderBoard;
--- frontend/src/Data/staticData.js
+++ frontend/src/Data/staticData.js
@@ -24,3 +24,10 @@ export const NAV_LINKS_DATA = [
     id: 4,
   },
 ];
+
+export const GLOBAL_LEADERBOARD = [
+  { rank: 1, player: "SpeedDemon", score: 2456, tops: 43, id: 1 },
+  { rank: 2, player: "JumpMaster", score: 2123, tops: 35, id: 2 },
+  { rank: 3, player: "Rocket", score: 1989, tops: 31, id: 3 },
+  { rank: 4, player: "BunnyHop", score: 1876, tops: 28, id: 4 },
+];
