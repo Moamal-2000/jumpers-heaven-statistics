337b72b1e7153a435946cd6afeca602fbffef6b8
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-16 18:44:19 +0300

    build/style: Introduction component home page

--- frontend/src/Components/Pages/Introduction/Introduction.jsx
+++ frontend/src/Components/Pages/Introduction/Introduction.jsx
@@ -1,7 +1,14 @@
 import s from "./Introduction.module.scss";
 
 const Introduction = () => {
-  return <section className={s.introduction}></section>;
+  return (
+    <section className={s.introduction}>
+      <h1>Global Rankings</h1>
+      <p>Top players ranked by overall performance and achievements</p>
+
+      {/* <SearchInput */}
+    </section>
+  );
 };
 
 export default Introduction;
