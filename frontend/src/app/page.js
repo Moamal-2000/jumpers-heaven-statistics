82e52535bfdc49c88ba856a1906b00aca991fdb3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-17 06:49:59 +0300

    updater: replace axios with fetch for speedrun leaderboard API call to revalidate data after 2 minutes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,15 @@
 import { jhApis } from "@/Api/jumpersHeaven";
 import Introduction from "@/Components/Pages/Home/Introduction/Introduction";
 import LeaderBoard from "@/Components/Pages/Home/LeaderBoard/LeaderBoard";
-import axios from "axios";
 import s from "./page.module.scss";
 
 export default async function Home() {
-  const speedRunLeaderboardReq = await axios(
-    jhApis({ fps: 125, limit: 20 }).speedRunLeaderboard
+  const speedRunLeaderboardReq = await fetch(
+    jhApis({ fps: 125, limit: 20 }).speedRunLeaderboard,
+    { next: { revalidate: 120 } }
   );
 
-  const speedRunLeaderboardData = await speedRunLeaderboardReq.data;
+  const speedRunLeaderboardData = await speedRunLeaderboardReq.json();
 
   return (
     <div className="container">
