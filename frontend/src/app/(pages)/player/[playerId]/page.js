dfb96ae04ac7ccda757012fa7edfb8f0f5e04294
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-17 07:04:52 +0300

    test: implement player details display with fetch from speedrun leaderboard API in player/id page

--- frontend/src/app/(pages)/player/[playerId]/page.js
+++ frontend/src/app/(pages)/player/[playerId]/page.js
@@ -1,7 +1,44 @@
+import { jhApis } from "@/Api/jumpersHeaven";
+import { getColoredName } from "@/Functions/utils";
+
 const PlayerId = async ({ params }) => {
   const { playerId } = await params;
 
-  return <main>PlayerId: {playerId}</main>;
+  const playersReq = await fetch(
+    jhApis({ fps: 125, limit: 20 }).speedRunLeaderboard,
+    { next: { revalidate: 120 } }
+  );
+  const playersData = await playersReq.json();
+  const playersDataArr = Object.values(playersData);
+  const playerData = playersDataArr.find(
+    ({ player_id }) => player_id === playerId
+  );
+  const { player_name, score, top_list } = playerData;
+
+  return (
+    <main>
+      <h2>Testing</h2>
+
+      <br />
+
+      <ul style={{ listStyleType: "none" }}>
+        <li>Player: {getColoredName(player_name)}</li>
+        <li>
+          Player ID: <span style={{ color: "yellow" }}>{playerId}</span>
+        </li>
+        <li>
+          Score: <span style={{ color: "orange" }}>{score}</span>
+        </li>
+        <li>
+          Tops: [
+          <span style={{ color: "cyan" }}>
+            {Object.values(top_list).join(", ")}
+          </span>
+          ]
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default PlayerId;
