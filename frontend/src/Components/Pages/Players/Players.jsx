84ff681af86acc16ff03a4d1c97ac0cfa1382e95
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-18 23:00:01 +0300

    feat: iterate on PLAYERS data to display players data in PlayerCard component

--- frontend/src/Components/Pages/Players/Players.jsx
+++ frontend/src/Components/Pages/Players/Players.jsx
@@ -1,7 +1,15 @@
+import { PLAYERS } from "@/Data/staticData";
+import PlayerCard from "./PlayerCard/PlayerCard";
 import s from "./Players.module.scss";
 
 const Players = () => {
-  return <section className={s.playersSection}></section>;
+  return (
+    <section className={s.playersSection}>
+      {PLAYERS.map((playerData) => (
+        <PlayerCard key={playerData.id} playerData={playerData} />
+      ))}
+    </section>
+  );
 };
 
 export default Players;
