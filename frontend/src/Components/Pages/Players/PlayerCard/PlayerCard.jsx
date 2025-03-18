43055a8bc341c804f173a34ce0f47b9e817cb1d5
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-18 22:54:32 +0300

    write: PlayerCard structure & props

--- frontend/src/Components/Pages/Players/PlayerCard/PlayerCard.jsx
+++ frontend/src/Components/Pages/Players/PlayerCard/PlayerCard.jsx
@@ -1,7 +1,41 @@
+import SvgIcon from "@/Components/Shared/SvgIcon";
+import Image from "next/image";
 import s from "./Player.module.scss";
 
-const PlayerCard = () => {
-  return <div className={s.playerCard}></div>;
+const PlayerCard = ({
+  playerData: { name, rank, avatar, totalPoints, completedMaps, bestTime },
+}) => {
+  return (
+    <div className={s.playerCard}>
+      <div className={s.mainInfo}>
+        <div className={s.avatar}>
+          {avatar && <Image width={64} height={64} alt={`${name} avatar`} />}
+          {!avatar && <SvgIcon name="users" />}
+        </div>
+
+        <div className={s.wrappers}>
+          <h2>{name}</h2>
+          <span>Rank #{rank}</span>
+        </div>
+      </div>
+
+      <div className={s.stats}>
+        <div className={s.stat}>
+          <p>Total Points:</p>
+          <span>{totalPoints}</span>
+        </div>
+
+        <div className={s.stat}>
+          <p>Maps Completed:</p> <span>{completedMaps}</span>
+        </div>
+
+        <div className={s.stat}>
+          <p>Best Time:</p>
+          <span>{bestTime}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlayerCard;
