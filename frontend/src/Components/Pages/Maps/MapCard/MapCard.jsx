ef48c76ddb51d50e2cecef69ae7842d0ebbc1e21
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-18 03:23:58 +0300

    update: iterate on MapCard component to display map cards

--- frontend/src/Components/Pages/Maps/MapCard/MapCard.jsx
+++ frontend/src/Components/Pages/Maps/MapCard/MapCard.jsx
@@ -1,9 +1,35 @@
+import SvgIcon from "@/Components/Shared/SvgIcon";
+import Image from "next/image";
 import s from "./MapCard.module.scss";
 
 const MapCard = ({
-  mapData: { name, description, bestTime, difficulty, img, id },
+  mapData: { name, description, bestTime, difficulty, img },
 }) => {
-  return <div className={s.card}></div>;
+  return (
+    <div className={s.mapCard}>
+      <div className={s.imgHolder}>
+        {img && <Image fill={true} alt={`${name} cod 2 map`} />}
+        {!img && <SvgIcon name="map" />}
+      </div>
+
+      <div className={s.mapInfo}>
+        <h2>{name}</h2>
+        <p>{description}</p>
+
+        <div className={s.mapDetails}>
+          <div className={s.bestTime}>
+            <SvgIcon name="timer" />
+            <p>Best Time: {bestTime}</p>
+          </div>
+
+          <div className={s.difficulty}>
+            <SvgIcon name="star" />
+            <p>Difficulty: {difficulty}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MapCard;
--- frontend/src/Components/Pages/Maps/Maps.jsx
+++ frontend/src/Components/Pages/Maps/Maps.jsx
@@ -1,34 +1,12 @@
-import SvgIcon from "@/Components/Shared/SvgIcon";
 import { MAPS } from "@/Data/staticData";
-import Image from "next/image";
+import MapCard from "./MapCard/MapCard";
 import s from "./Maps.module.scss";
 
 const Maps = () => {
   return (
     <section className={s.mapsSection}>
-      {MAPS.map(({ name, description, bestTime, difficulty, img, id }) => (
-        <div key={id} className={s.mapCard}>
-          <div className={s.imgHolder}>
-            {img && <Image fill={true} alt={`${name} cod 2 map`} />}
-            {!img && <SvgIcon name="map" />}
-          </div>
-
-          <div className={s.mapInfo}>
-            <h2>{name}</h2>
-            <p>{description}</p>
-
-            <div className={s.mapDetails}>
-              <div className={s.bestTime}>
-                <SvgIcon name="timer" />
-                <p>Best Time: {bestTime}</p>
-              </div>
-              <div className={s.difficulty}>
-                <SvgIcon name="star" />
-                <p>Difficulty: {difficulty}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+      {MAPS.map((mapData) => (
+        <MapCard key={mapData.id} mapData={mapData} />
       ))}
     </section>
   );
