206e2624f1de06238ceb669d5f1d953e600057aa
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-19 18:10:53 +0300

    fix: update score column header to "Total score" for clarity in LeaderBoard component

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
@@ -1,14 +1,14 @@
 import s from "./LeaderBoard.module.scss";
 import LeaderBoardTBody from "./LeaderBoardTBody/LeaderBoardTBody";
 
-const LeaderBoard = ({leaderboardData}) => {
+const LeaderBoard = ({ leaderboardData }) => {
   return (
     <table className={s.leaderBoard}>
       <thead>
         <tr>
           <th className={s.rank}>Rank</th>
           <th className={s.player}>Player</th>
-          <th className={s.score}>Score</th>
+          <th className={s.score}>Total score</th>
           <th className={s.tops}>Tops 1-10</th>
         </tr>
       </thead>
