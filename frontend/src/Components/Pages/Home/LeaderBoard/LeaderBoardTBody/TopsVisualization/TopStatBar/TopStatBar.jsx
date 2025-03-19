d02a07be4b4a156ab96b2de46ec169e4112e1703
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-19 20:10:19 +0300

    style: enhance TopStatBar components for improved layout and responsiveness

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTBody/TopsVisualization/TopStatBar/TopStatBar.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTBody/TopsVisualization/TopStatBar/TopStatBar.jsx
@@ -1,7 +1,14 @@
 import s from "./TopStatBar.module.scss";
 
+const topStatColors = ["#ffd700", "#c0c0c0", "#cd7f32"];
+const jumpersHeavenMapCount = 560;
+
 const TopStatBar = ({ top, times }) => {
-  const statColors = ["#ffd700", "#c0c0c0", "#cd7f32"];
+  const statsBarStyles = {
+    backgroundColor: topStatColors[top - 1],
+    opacity: 0.8,
+    height: "640px",
+  };
 
   return (
     <div className={s.statBarWrapper}>
@@ -9,13 +16,10 @@ const TopStatBar = ({ top, times }) => {
         {times} times in position {top}
       </p>
 
-      <div
-        className={s.statBar}
-        style={{ backgroundColor: statColors[top - 1] }}
-      >
-        <span className={s.top}>#{top}</span>
-        <span className={s.times}>{times}</span>
-      </div>
+      <span className={s.top}>#{top}</span>
+      <span className={s.times}>{times}</span>
+
+      <div className={s.statBar} style={statsBarStyles} />
     </div>
   );
 };
