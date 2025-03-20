360e950acdcc340138275c89c62e8e98ac6e43fb
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-20 06:37:07 +0300

    feat: show only 1, 2, 3 top stats bar in TopsVisualization component

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTBody/TopsVisualization/TopsVisualization.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTBody/TopsVisualization/TopsVisualization.jsx
@@ -1,22 +1,30 @@
+"use client";
+
 import { getMaxFinishTimesFrom } from "@/Functions/utils";
+import { useState } from "react";
 import TopStatBar from "./TopStatBar/TopStatBar";
 import s from "./TopsVisualization.module.scss";
 
 const TopsVisualization = ({ topsList, mapsCount, leaderboardData }) => {
+  const [showMoreStats, setShowMoreStats] = useState(false);
   const maxFinishTimes = getMaxFinishTimesFrom(leaderboardData[0]);
   const topsEntries = Object.entries(topsList);
 
   return (
     <div className={s.tops}>
-      {topsEntries.map((topStat, index) => (
-        <TopStatBar
-          top={topStat[0]}
-          times={topStat[1]}
-          mapsCount={mapsCount}
-          maxFinishTimes={maxFinishTimes}
-          key={`stat-bar-${index}`}
-        />
-      ))}
+      {topsEntries.map((topStat, index) => {
+        if (!showMoreStats && index >= 3) return;
+
+        return (
+          <TopStatBar
+            top={topStat[0]}
+            times={topStat[1]}
+            mapsCount={mapsCount}
+            maxFinishTimes={maxFinishTimes}
+            key={`stat-bar-${index}`}
+          />
+        );
+      })}
     </div>
   );
 };
