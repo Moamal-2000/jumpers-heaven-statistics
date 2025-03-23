f199a4a6b3bbd1f7ef723b951a44e754541a6b13
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-23 03:54:26 +0300

    feat: display filter cards in FiltersSection component

--- frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
@@ -1,7 +1,17 @@
+import FilterCard from "@/Components/Shared/Cards/FilterCard/FilterCard";
 import s from "./FiltersSection.module.scss";
 
 const FiltersSection = () => {
-  return <section className={s.filtersSection}></section>;
+  return (
+    <section className={s.filtersSection}>
+      <FilterCard title="Leaderboard Type"></FilterCard>
+      <FilterCard title="FPS Status"></FilterCard>
+      <FilterCard title="Players Region"></FilterCard>
+      <FilterCard title="Last seen"></FilterCard>
+      <FilterCard title="Player Status"></FilterCard>
+      <FilterCard title="Stats Filter"></FilterCard>
+    </section>
+  );
 };
 
 export default FiltersSection;
