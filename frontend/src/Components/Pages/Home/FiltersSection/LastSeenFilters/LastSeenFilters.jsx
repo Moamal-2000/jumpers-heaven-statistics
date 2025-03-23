bfb36a023bc991e1e4ecb8a3910efce7dd91fba6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-23 06:21:06 +0300

    feat: implement dynamic url query for last-seen in LastSeenFilters component

--- frontend/src/Components/Pages/Home/FiltersSection/LastSeenFilters/LastSeenFilters.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/LastSeenFilters/LastSeenFilters.jsx
@@ -1,7 +1,45 @@
+"use client";
+
+import FilterButton from "@/Components/Shared/Buttons/FilterButton/FilterButton";
+import { useSearchParams } from "next/navigation";
 import s from "./LastSeenFilters.module.scss";
 
 const LastSeenFilters = () => {
-  return <div className={s.filters}></div>;
+  const searchParams = useSearchParams();
+  const lastSeenUrlQuery = searchParams.get("last-seen");
+
+  return (
+    <div className={s.filters}>
+      {LAST_SEEN_FILTERS_DATA.map(({ text, id }) => (
+        <FilterButton
+          key={id}
+          text={text}
+          queryName="last-seen"
+          urlQuery={lastSeenUrlQuery}
+          defaultUrlQuery="all time"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default LastSeenFilters;
+
+const LAST_SEEN_FILTERS_DATA = [
+  {
+    text: "All time",
+    id: 1,
+  },
+  {
+    text: "This Month",
+    id: 2,
+  },
+  {
+    text: "This Week",
+    id: 3,
+  },
+  {
+    text: "Today",
+    id: 4,
+  },
+];
