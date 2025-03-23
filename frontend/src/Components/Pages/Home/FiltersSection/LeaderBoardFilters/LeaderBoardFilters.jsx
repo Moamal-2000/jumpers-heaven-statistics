c482661e986aa50c942a3cd8f018d4fa12ffbbc8
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-23 05:23:28 +0300

    init: display LeaderBoardFilters filter buttons

--- frontend/src/Components/Pages/Home/FiltersSection/LeaderBoardFilters/LeaderBoardFilters.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/LeaderBoardFilters/LeaderBoardFilters.jsx
@@ -1,7 +1,30 @@
 import s from "./LeaderBoardFilters.module.scss";
 
 const LeaderBoardFilters = () => {
-  return <div className={s.filters}></div>;
+  return (
+    <div className={s.filters}>
+      {LEADERBOARD_FILTERS_DATA.map(({ text, id }) => (
+        <button type="button" key={id}>
+          {text}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default LeaderBoardFilters;
+
+const LEADERBOARD_FILTERS_DATA = [
+  {
+    text: "All",
+    id: 1,
+  },
+  {
+    text: "Speedrun",
+    id: 2,
+  },
+  {
+    text: "Skilled",
+    id: 3,
+  },
+];
