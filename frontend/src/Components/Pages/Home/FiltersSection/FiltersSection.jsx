7c343cacc57653683ddaeacdd9eeb738a4314436
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-23 04:01:36 +0300

    init: LeaderBoardFilters component

--- frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
@@ -1,10 +1,13 @@
 import FilterCard from "@/Components/Shared/Cards/FilterCard/FilterCard";
 import s from "./FiltersSection.module.scss";
+import LeaderBoardFilters from "./LeaderBoardFilters/LeaderBoardFilters";
 
 const FiltersSection = () => {
   return (
     <section className={s.filtersSection}>
-      <FilterCard title="Leaderboard Type"></FilterCard>
+      <FilterCard title="Leaderboard Type">
+        <LeaderBoardFilters />
+      </FilterCard>
       <FilterCard title="FPS Status"></FilterCard>
       <FilterCard title="Players Region"></FilterCard>
       <FilterCard title="Last seen"></FilterCard>
---
+++ frontend/src/Components/Pages/Home/FiltersSection/LeaderBoardFilters/LeaderBoardFilters.jsx
@@ -0,0 +1,7 @@
+import s from "./LeaderBoardFilters.module.scss";
+
+const LeaderBoardFilters = () => {
+  return <div className={s.filters}></div>;
+};
+
+export default LeaderBoardFilters;
