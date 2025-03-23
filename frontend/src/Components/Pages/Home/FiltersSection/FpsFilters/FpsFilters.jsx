1503350ba2569ad1a9dfac077c3e4b6cbe9228db
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-23 06:15:08 +0300

    feat: implement dynamic url query for fps in FpsFilters component

--- frontend/src/Components/Pages/Home/FiltersSection/FpsFilters/FpsFilters.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/FpsFilters/FpsFilters.jsx
@@ -1,7 +1,53 @@
+"use client";
+
+import FilterButton from "@/Components/Shared/Buttons/FilterButton/FilterButton";
+import { useSearchParams } from "next/navigation";
 import s from "./FpsFilters.module.scss";
 
 const FpsFilters = () => {
-  return <div className={s.filters}></div>;
+  const searchParams = useSearchParams();
+  const fpsUrlQuery = searchParams.get("fps");
+
+  return (
+    <div className={s.filters}>
+      {FPS_FILTERS_DATA.map(({ text, id }) => (
+        <FilterButton
+          key={id}
+          text={text}
+          queryName="fps"
+          urlQuery={fpsUrlQuery}
+          defaultUrlQuery="mix"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default FpsFilters;
+
+const FPS_FILTERS_DATA = [
+  {
+    text: "Mix",
+    id: 1,
+  },
+  {
+    text: "43",
+    id: 2,
+  },
+  {
+    text: "76",
+    id: 3,
+  },
+  {
+    text: "125",
+    id: 4,
+  },
+  {
+    text: "250",
+    id: 5,
+  },
+  {
+    text: "333",
+    id: 6,
+  },
+];
