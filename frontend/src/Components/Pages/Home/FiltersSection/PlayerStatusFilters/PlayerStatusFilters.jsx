749a8d78e6787ea7cab1b136637898469e80bb18
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-23 20:49:41 +0300

    init: CustomCheckbox component

--- frontend/src/Components/Pages/Home/FiltersSection/PlayerStatusFilters/PlayerStatusFilters.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/PlayerStatusFilters/PlayerStatusFilters.jsx
@@ -1,7 +1,12 @@
+import CustomCheckbox from "@/Components/Shared/Checkboxes/CustomCheckbox/CustomCheckbox";
 import s from "./PlayerStatusFilters.module.scss";
 
 const PlayerStatusFilters = () => {
-  return <div className={s.filters}></div>;
+  return (
+    <div className={s.filters}>
+      <CustomCheckbox />
+    </div>
+  );
 };
 
 export default PlayerStatusFilters;
---
+++ frontend/src/Components/Shared/Checkboxes/CustomCheckbox/CustomCheckbox.jsx
@@ -0,0 +1,7 @@
+import s from "./CustomCheckbox.module.scss";
+
+const CustomCheckbox = () => {
+  return <div className={s.customCheckbox}></div>;
+};
+
+export default CustomCheckbox;
