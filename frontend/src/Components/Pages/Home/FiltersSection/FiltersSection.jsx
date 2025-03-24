8a4bbd64e589104e7e2469e74eda1b543c61f30f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-24 06:41:22 +0300

    refactor: simplify FiltersSection component by mapping on filter components

--- frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
@@ -10,31 +10,46 @@ import StatsFilters from "./StatsFilters/StatsFilters";
 const FiltersSection = () => {
   return (
     <section className={s.filtersSection}>
-      <FilterCard title="Leaderboard Type">
-        <LeaderBoardFilters />
-      </FilterCard>
-
-      <FilterCard title="FPS Status">
-        <FpsFilters />
-      </FilterCard>
-
-      <FilterCard title="Players Region">
-        <RegionFilters />
-      </FilterCard>
-
-      <FilterCard title="Last seen">
-        <LastSeenFilters />
-      </FilterCard>
-
-      <FilterCard title="Player Status">
-        <PlayerStatusFilters />
-      </FilterCard>
-
-      <FilterCard title="Stats Filter">
-        <StatsFilters />
-      </FilterCard>
+      {FILTERS_COMPONENTS_DATA.map(({ title, filtersComponent, id }) => (
+        <FilterCard key={id} title={title}>
+          {filtersComponent}
+        </FilterCard>
+      ))}
     </section>
   );
 };
 
 export default FiltersSection;
+
+const FILTERS_COMPONENTS_DATA = [
+  {
+    title: "Leaderboard Type",
+    filtersComponent: <LeaderBoardFilters />,
+    id: 1,
+  },
+  {
+    title: "FPS Status",
+    filtersComponent: <FpsFilters />,
+    id: 2,
+  },
+  {
+    title: "Players Region",
+    filtersComponent: <RegionFilters />,
+    id: 3,
+  },
+  {
+    title: "Last seen",
+    filtersComponent: <LastSeenFilters />,
+    id: 4,
+  },
+  {
+    title: "Player Status",
+    filtersComponent: <PlayerStatusFilters />,
+    id: 5,
+  },
+  {
+    title: "Stats Filter",
+    filtersComponent: <StatsFilters />,
+    id: 6,
+  },
+];
