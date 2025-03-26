2c907f4615e0f2b5e3abf98f028879087d66ec18
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-26 10:00:39 +0300

    refactor: remove Suspense wrapper from FilterCard component

--- frontend/src/Components/Shared/Cards/FilterCard/FilterCard.jsx
+++ frontend/src/Components/Shared/Cards/FilterCard/FilterCard.jsx
@@ -1,14 +1,11 @@
-import { Suspense } from "react";
 import s from "./FilterCard.module.scss";
 
 const FilterCard = ({ title, children }) => {
   return (
-    <Suspense>
-      <div className={s.card}>
-        <h2>{title}</h2>
-        {children}
-      </div>
-    </Suspense>
+    <div className={s.card}>
+      <h2>{title}</h2>
+      {children}
+    </div>
   );
 };
 
