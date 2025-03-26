4194444072a26b6e2680dc96d1958581ce9e8251
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-26 08:48:27 +0300

    feat: update LeaderBoard component to display dynamic header based on leaderboard type

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 import s from "./LeaderBoard.module.scss";
 import LeaderBoardTBody from "./LeaderBoardTBody/LeaderBoardTBody";
 
 const LeaderBoard = ({ leaderboardData, mapsCount }) => {
   const [isReverseTable, setIsReverseTable] = useState(false);
+  const searchParams = useSearchParams();
+  const leaderboardType = searchParams.get("leaderboard");
+  const isSkilledLeaderboard = leaderboardType === "skilled";
 
   function toggleReverseTable() {
     setIsReverseTable((prevState) => !prevState);
@@ -18,7 +22,9 @@ const LeaderBoard = ({ leaderboardData, mapsCount }) => {
           <th className={s.rank}>Rank</th>
           <th className={s.player}>Player</th>
           <th className={s.score}>Total score</th>
-          <th className={s.tops}>Tops 1-10</th>
+          <th className={s.tops}>
+            {isSkilledLeaderboard ? "points difficulty" : "Tops 1-10"}
+          </th>
         </tr>
       </thead>
 
