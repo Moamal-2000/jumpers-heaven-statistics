66e4360b2ff0a673e220d02fd218aada3a6cc19e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-27 19:36:06 +0300

    write: "not found page" structure content & CSS classes

--- frontend/src/app/not-found.js
+++ frontend/src/app/not-found.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { updateGlobalState } from "@/Redux/slices/globalSlice";
+import Link from "next/link";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import s from "./NotFoundPage.module.scss";
@@ -15,7 +16,17 @@ const NotFoundPage = () => {
       dispatch(updateGlobalState({ key: "isNotFoundPage", value: false }));
   }, [dispatch]);
 
-  return <main className={s.notFoundPage}></main>;
+  return (
+    <main className={s.notFoundPage}>
+      <h1 className={s.text404}>404</h1>
+      <h2 className={s.title}>Page Not Found</h2>
+      <p className={s.description}>
+        {`The page you're looking for doesn't exist or has been moved. Don't
+        worry, we'll help you get back to the main site or find what you need.`}
+      </p>
+      <Link href="/" className={s.returnHomeBtn}>Return to Home page</Link>
+    </main>
+  );
 };
 
 export default NotFoundPage;
