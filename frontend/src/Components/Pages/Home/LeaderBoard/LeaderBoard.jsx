794d9abf017e2d6f8b6a3457ae7ead795aa18ebc
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-27 14:27:22 +0300

    feat: implement leaderboard fetching and state management in globalSlice

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoard.jsx
@@ -1,12 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { fetchLeaderboard } from "@/Redux/slices/globalSlice";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import s from "./LeaderBoard.module.scss";
 import LeaderBoardTBody from "./LeaderBoardTBody/LeaderBoardTBody";
 import LeaderBoardTHead from "./LeaderBoardTHead/LeaderBoardTHead";
 
 const LeaderBoard = ({ leaderboardData, mapsCount }) => {
   const [isReverseTable, setIsReverseTable] = useState(false);
+  const { leaderboard } = useSelector((s) => s.global);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (leaderboard.length !== 0) return;
+
+    dispatch(fetchLeaderboard());
+  }, []);
 
   return (
     <table className={s.leaderBoard}>
--- frontend/src/Redux/slices/globalSlice.js
+++ frontend/src/Redux/slices/globalSlice.js
@@ -1,11 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { jhApis } from "@/Api/jumpersHeaven";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  leaderboard: [],
+};
+
+export const fetchLeaderboard = createAsyncThunk(
+  "data/fetchLeaderboard",
+  async () => {
+    const res = await fetch(jhApis({ limit: 20 }).speedRunLeaderboard);
+
+    const data = await res.json();
+    return data;
+  }
+);
 
 export const globalSlice = createSlice({
   initialState,
   name: "globalSlice",
   reducers: {},
+  extraReducers: ({ addCase }) => {
+    addCase(fetchLeaderboard.fulfilled, (state, action) => {
+      state.leaderboard = action.payload;
+    });
+  },
 });
 
 export default globalSlice.reducer;
