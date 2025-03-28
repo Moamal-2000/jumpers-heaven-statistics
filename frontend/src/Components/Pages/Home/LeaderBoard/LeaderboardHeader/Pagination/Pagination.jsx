0ad23b95d1797a059eb0f2e1836e22fc44b79eb4
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-28 09:34:24 +0300

    feat: display dynamic pagination buttons in Pagination component

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { PAGINATION_ITEMS_PER_PAGE } from "@/Data/constants";
 import s from "./Pagination.module.scss";
 
 const Pagination = ({ data }) => {
   const numberOfPages = Math.ceil(data?.length / PAGINATION_ITEMS_PER_PAGE);
+  const paginationButtons = Array(numberOfPages).fill();
 
   return (
     <div className={s.pagination}>
@@ -10,17 +13,19 @@ const Pagination = ({ data }) => {
         ←
       </button>
 
-      <button type="button" className={s.numberButton}>
-        1
-      </button>
+      {paginationButtons.map((_, index) => {
+        const activeClass = index === 0 ? s.active : "";
 
-      <button type="button" className={s.numberButton}>
-        2
-      </button>
-
-      <button type="button" className={s.numberButton}>
-        3
-      </button>
+        return (
+          <button
+            type="button"
+            key={index}
+            className={`${s.numberButton} ${activeClass}`}
+          >
+            {index + 1}
+          </button>
+        );
+      })}
 
       <button type="button" className={s.arrowButton}>
         →
