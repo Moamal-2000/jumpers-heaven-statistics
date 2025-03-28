6f8d0ff2075bda9c1df39388b75fbc57317389c0
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-28 09:07:24 +0300

    init: Pagination component for Leaderboard table

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeader.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeader.jsx
@@ -1,10 +1,11 @@
 import s from "./LeaderboardHeader.module.scss";
+import Pagination from "./Pagination/Pagination";
 
 const LeaderboardHeader = () => {
   return (
     <header className={s.header}>
       <h3>Top Players</h3>
-      {/* <Pagination /> */}
+      <Pagination />
     </header>
   );
 };
---
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
@@ -0,0 +1,7 @@
+import s from "./Pagination.module.scss";
+
+const Pagination = () => {
+  return <div className={s.pagination}>Pagination</div>;
+};
+
+export default Pagination;
