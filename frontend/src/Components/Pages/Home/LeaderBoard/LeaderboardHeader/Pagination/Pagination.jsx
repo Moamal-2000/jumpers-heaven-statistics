24fc60ddc4ffba761a2519622c980485db0c5a3f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-28 09:45:08 +0300

    feat: update URL with query parameter when user clicks pagination numbers

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
@@ -1,11 +1,27 @@
 "use client";
 
 import { PAGINATION_ITEMS_PER_PAGE } from "@/Data/constants";
+import { createQueryString } from "@/Functions/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import s from "./Pagination.module.scss";
 
 const Pagination = ({ data }) => {
   const numberOfPages = Math.ceil(data?.length / PAGINATION_ITEMS_PER_PAGE);
   const paginationButtons = Array(numberOfPages).fill();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const activePagination = searchParams.get("leaderboard-pagination");
+
+  function changePage(pageNumber) {
+    createQueryString(
+      "leaderboard-pagination",
+      pageNumber,
+      searchParams,
+      router,
+      pathname
+    );
+  }
 
   return (
     <div className={s.pagination}>
@@ -14,13 +30,18 @@ const Pagination = ({ data }) => {
       </button>
 
       {paginationButtons.map((_, index) => {
-        const activeClass = index === 0 ? s.active : "";
+        const isActiveButton = +activePagination === index + 1;
+        const isFirstButton = index + 1 === 1;
+        const shouldBeActive =
+          isActiveButton || (isFirstButton && !activePagination);
+        const activeClass = shouldBeActive ? s.active : "";
 
         return (
           <button
             type="button"
             key={index}
             className={`${s.numberButton} ${activeClass}`}
+            onClick={() => changePage(index + 1)}
           >
             {index + 1}
           </button>
