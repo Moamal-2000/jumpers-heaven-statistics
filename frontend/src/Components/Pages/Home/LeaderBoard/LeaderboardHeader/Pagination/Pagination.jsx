4c89a3cd679d47b941e4dd14e996fdbb3889b159
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-28 09:27:07 +0300

    write: pagination buttons in Pagination component

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/Pagination/Pagination.jsx
@@ -4,7 +4,29 @@ import s from "./Pagination.module.scss";
 const Pagination = ({ data }) => {
   const numberOfPages = Math.ceil(data?.length / PAGINATION_ITEMS_PER_PAGE);
 
-  return <div className={s.pagination}></div>;
+  return (
+    <div className={s.pagination}>
+      <button type="button" className={s.arrowButton}>
+        ←
+      </button>
+
+      <button type="button" className={s.numberButton}>
+        1
+      </button>
+
+      <button type="button" className={s.numberButton}>
+        2
+      </button>
+
+      <button type="button" className={s.numberButton}>
+        3
+      </button>
+
+      <button type="button" className={s.arrowButton}>
+        →
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
