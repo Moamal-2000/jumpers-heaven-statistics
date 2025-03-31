ba5dd34f6dadd149166939df299e658e900bf623
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-31 10:08:57 +0300

    remove: pagination component from leaderboard header

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeader.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeader.jsx
@@ -1,14 +1,9 @@
-import { useSelector } from "react-redux";
 import s from "./LeaderboardHeader.module.scss";
-import Pagination from "./Pagination/Pagination";
 
 const LeaderboardHeader = () => {
-  const { leaderboardData } = useSelector((s) => s.leaderboard);
-
   return (
     <header className={s.header}>
       <h3>Top Players</h3>
-      <Pagination data={leaderboardData} />
     </header>
   );
 };
