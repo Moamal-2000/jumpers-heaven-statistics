457cc421789814b6a3323094753779d7a7981d00
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-31 07:39:30 +0300

    refactor: move FILTERS_COMPONENTS_DATA to a separate file for better organization

---
+++ frontend/src/Data/filters.jsx
@@ -0,0 +1,117 @@
+import PlayerStatusFilters from "@/Components/Pages/Home/FiltersSection/PlayerStatusFilters/PlayerStatusFilters";
+
+export const FILTERS_COMPONENTS_DATA = [
+  {
+    title: "Leaderboard Type",
+    queryName: "leaderboard",
+    defaultUrlQuery: "speedrun",
+    filtersData: [
+      {
+        text: "Speedrun",
+        id: 1,
+      },
+      {
+        text: "Skilled",
+        id: 2,
+      },
+      {
+        text: "Defrag",
+        id: 3,
+      },
+      {
+        text: "Surf",
+        id: 4,
+      },
+    ],
+    id: 1,
+  },
+  {
+    title: "FPS Status",
+    queryName: "fps",
+    defaultUrlQuery: "125",
+    filtersData: [
+      {
+        text: "43",
+        id: 1,
+      },
+      {
+        text: "76",
+        id: 2,
+      },
+      {
+        text: "125",
+        id: 3,
+      },
+      {
+        text: "250",
+        id: 4,
+      },
+      {
+        text: "333",
+        id: 5,
+      },
+      {
+        text: "Mix",
+        id: 6,
+      },
+    ],
+    id: 2,
+  },
+  {
+    title: "Players Region",
+    queryName: "Region",
+    defaultUrlQuery: "global",
+    filtersData: [
+      {
+        text: "Global",
+        id: 1,
+      },
+      {
+        text: "NA",
+        id: 2,
+      },
+      {
+        text: "EU",
+        id: 3,
+      },
+      {
+        text: "ASIA",
+        id: 4,
+      },
+      {
+        text: "OCE",
+        id: 5,
+      },
+    ],
+    id: 3,
+  },
+  {
+    title: "Last seen",
+    queryName: "last-seen",
+    defaultUrlQuery: "All time",
+    filtersData: [
+      {
+        text: "All time",
+        id: 1,
+      },
+      {
+        text: "This Month",
+        id: 2,
+      },
+      {
+        text: "This Week",
+        id: 3,
+      },
+      {
+        text: "Today",
+        id: 4,
+      },
+    ],
+    id: 4,
+  },
+  {
+    title: "Player Status",
+    filtersComponent: <PlayerStatusFilters />,
+    id: 5,
+  },
+];
