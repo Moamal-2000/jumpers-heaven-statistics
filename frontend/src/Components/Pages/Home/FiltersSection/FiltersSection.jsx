6e6121bad98687f6010e86c5923854bf439ba773
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-31 07:37:42 +0300

    update: integrate FilterButtons into FiltersSection for improved filter management

--- frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Home/FiltersSection/FiltersSection.jsx
@@ -1,12 +1,9 @@
 "use client";
 
 import FilterCard from "@/Components/Shared/Cards/FilterCard/FilterCard";
+import FilterButtons from "./FilterButtons/FilterButtons";
 import s from "./FiltersSection.module.scss";
-import FpsFilters from "./FpsFilters/FpsFilters";
-import LastSeenFilters from "./LastSeenFilters/LastSeenFilters";
-import LeaderBoardFilters from "./LeaderBoardFilters/LeaderBoardFilters";
 import PlayerStatusFilters from "./PlayerStatusFilters/PlayerStatusFilters";
-import RegionFilters from "./RegionFilters/RegionFilters";
 
 const FiltersSection = () => {
   // const searchParams = useSearchParams();
@@ -35,11 +32,33 @@ const FiltersSection = () => {
 
   return (
     <section className={s.filtersSection}>
-      {FILTERS_COMPONENTS_DATA.map(({ title, filtersComponent, id }) => (
-        <FilterCard key={id} title={title}>
-          {filtersComponent}
-        </FilterCard>
-      ))}
+      {FILTERS_COMPONENTS_DATA.map(
+        ({
+          title,
+          queryName,
+          defaultUrlQuery,
+          filtersData,
+          id,
+          filtersComponent,
+        }) => {
+          if (filtersComponent)
+            return (
+              <FilterCard key={id} title={title}>
+                {filtersComponent}
+              </FilterCard>
+            );
+
+          return (
+            <FilterCard key={id} title={title}>
+              <FilterButtons
+                queryName={queryName.toLowerCase()}
+                defaultUrlQuery={defaultUrlQuery.toLowerCase()}
+                filtersData={filtersData}
+              />
+            </FilterCard>
+          );
+        }
+      )}
     </section>
   );
 };
@@ -49,22 +68,110 @@ export default FiltersSection;
 const FILTERS_COMPONENTS_DATA = [
   {
     title: "Leaderboard Type",
-    filtersComponent: <LeaderBoardFilters />,
+    queryName: "leaderboard",
+    defaultUrlQuery: "speedrun",
+    filtersData: [
+      {
+        text: "Speedrun",
+        id: 1,
+      },
+      {
+        text: "Skilled",
+        id: 2,
+      },
+      {
+        text: "Defrag",
+        id: 3,
+      },
+      {
+        text: "Surf",
+        id: 4,
+      },
+    ],
     id: 1,
   },
   {
     title: "FPS Status",
-    filtersComponent: <FpsFilters />,
+    queryName: "fps",
+    defaultUrlQuery: "125",
+    filtersData: [
+      {
+        text: "43",
+        id: 1,
+      },
+      {
+        text: "76",
+        id: 2,
+      },
+      {
+        text: "125",
+        id: 3,
+      },
+      {
+        text: "250",
+        id: 4,
+      },
+      {
+        text: "333",
+        id: 5,
+      },
+      {
+        text: "Mix",
+        id: 6,
+      },
+    ],
     id: 2,
   },
   {
     title: "Players Region",
-    filtersComponent: <RegionFilters />,
+    queryName: "Region",
+    defaultUrlQuery: "global",
+    filtersData: [
+      {
+        text: "Global",
+        id: 1,
+      },
+      {
+        text: "NA",
+        id: 2,
+      },
+      {
+        text: "EU",
+        id: 3,
+      },
+      {
+        text: "ASIA",
+        id: 4,
+      },
+      {
+        text: "OCE",
+        id: 5,
+      },
+    ],
     id: 3,
   },
   {
     title: "Last seen",
-    filtersComponent: <LastSeenFilters />,
+    queryName: "last-seen",
+    defaultUrlQuery: "All time",
+    filtersData: [
+      {
+        text: "All time",
+        id: 1,
+      },
+      {
+        text: "This Month",
+        id: 2,
+      },
+      {
+        text: "This Week",
+        id: 3,
+      },
+      {
+        text: "Today",
+        id: 4,
+      },
+    ],
     id: 4,
   },
   {
