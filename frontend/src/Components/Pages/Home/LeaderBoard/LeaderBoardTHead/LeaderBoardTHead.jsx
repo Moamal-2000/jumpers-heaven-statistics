f0b588e36e5df36d79f66aa7fc1f6960792636ec
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-02 10:56:06 +0300

    write: RankTableHeader logic

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTHead/LeaderBoardTHead.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTHead/LeaderBoardTHead.jsx
@@ -1,40 +1,18 @@
 "use client";
 
-import { updateGlobalState } from "@/Redux/slices/globalSlice";
 import { useSearchParams } from "next/navigation";
-import { useDispatch, useSelector } from "react-redux";
 import s from "./LeaderBoardTHead.module.scss";
+import RankTableHeader from "./RankTableHeader/RankTableHeader";
 
 const LeaderBoardTHead = () => {
-  const { isLeaderboardReversed } = useSelector((s) => s.global);
-  const dispatch = useDispatch();
   const searchParams = useSearchParams();
   const leaderboardType = searchParams.get("leaderboard");
   const isSkilledLeaderboard = leaderboardType === "skilled";
 
-  function reverseLeaderboard() {
-    dispatch(
-      updateGlobalState({
-        key: "isLeaderboardReversed",
-        value: !isLeaderboardReversed,
-      })
-    );
-  }
-
   return (
     <thead className={s.thead}>
       <tr>
-        <th
-          className={s.rank}
-          data-sortable
-          tabIndex="0"
-          aria-label={`Sort leaderboard by rank ${
-            isLeaderboardReversed ? "descending" : "ascending"
-          }`}
-          onClick={reverseLeaderboard}
-        >
-          <span>Rank</span>
-        </th>
+        <RankTableHeader text="Rank" />
         <th className={s.player}>Player</th>
         <th className={s.rating}>Rating</th>
         <th className={s.score}>Points</th>
--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTHead/RankTableHeader/RankTableHeader.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderBoardTHead/RankTableHeader/RankTableHeader.jsx
@@ -1,5 +1,35 @@
-const RankTableHeader = () => {
-  return <th></th>;
+"use client";
+
+import { updateGlobalState } from "@/Redux/slices/globalSlice";
+import { useDispatch, useSelector } from "react-redux";
+import s from "./RankTableHeader.module.scss";
+
+const RankTableHeader = ({ text }) => {
+  const { isLeaderboardReversed } = useSelector((s) => s.global);
+  const dispatch = useDispatch();
+
+  function reverseLeaderboard() {
+    dispatch(
+      updateGlobalState({
+        key: "isLeaderboardReversed",
+        value: !isLeaderboardReversed,
+      })
+    );
+  }
+
+  return (
+    <th
+      className={s.rank}
+      data-sortable
+      tabIndex="0"
+      aria-label={`Sort leaderboard by rank ${
+        isLeaderboardReversed ? "descending" : "ascending"
+      }`}
+      onClick={reverseLeaderboard}
+    >
+      <span>{text}</span>
+    </th>
+  );
 };
 
 export default RankTableHeader;
