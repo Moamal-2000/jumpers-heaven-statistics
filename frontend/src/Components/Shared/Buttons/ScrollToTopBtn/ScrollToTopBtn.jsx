9dbd3a8c3ef0f0fbac021658dd94dc4b58e5b6a5
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-02 09:45:47 +0300

    style: ScrollToTopBtn with hover effects

--- frontend/src/Components/Shared/Buttons/ScrollToTopBtn/ScrollToTopBtn.jsx
+++ frontend/src/Components/Shared/Buttons/ScrollToTopBtn/ScrollToTopBtn.jsx
@@ -1,7 +1,12 @@
+import SvgIcon from "../../SvgIcon";
 import s from "./ScrollToTopBtn.module.scss";
 
 const ScrollToTopBtn = () => {
-  return <button type="button" className={s.button}></button>;
+  return (
+    <button type="button" className={s.button}>
+      <SvgIcon name="right-arrow" />
+    </button>
+  );
 };
 
 export default ScrollToTopBtn;
