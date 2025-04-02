b36a3d24deaa6b655b19d5f659fcf213658c6f8a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-02 09:50:27 +0300

    add: helper custom hooks to the project

---
+++ frontend/src/Hooks/Helper/useCopyText.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
+const useCopyText = () => {
+  const [copiedText, setCopiedText] = useState("");
+
+  function setCopy(text) {
+    navigator.clipboard.writeText(text);
+    setCopiedText(text);
+  }
+
+  return [copiedText, setCopy];
+};
+
+export default useCopyText;
---
+++ frontend/src/Hooks/Helper/useGetSearchParam.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useSearchParams } from "react-router-dom";
+
+const useGetSearchParam = (key) => {
+  const [searchParams] = useSearchParams();
+  const searchParam = searchParams.get(key);
+  return searchParam;
+};
+export default useGetSearchParam;
