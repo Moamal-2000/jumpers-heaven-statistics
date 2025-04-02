642557585020918bfb566ef2acb27bc99bb3146a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-02 10:00:53 +0300

    feat: show ScrollToTopBtn based on scroll position

--- frontend/src/Components/Shared/Buttons/ScrollToTopBtn/ScrollToTopBtn.jsx
+++ frontend/src/Components/Shared/Buttons/ScrollToTopBtn/ScrollToTopBtn.jsx
@@ -1,9 +1,26 @@
+"use client";
+
+import useEventListener from "@/Hooks/Helper/useEventListener";
+import { useState } from "react";
 import SvgIcon from "../../SvgIcon";
 import s from "./ScrollToTopBtn.module.scss";
 
 const ScrollToTopBtn = () => {
+  const [displayButton, setDisplayButton] = useState(false);
+  const activeClass = displayButton ? s.active : "";
+
+  useEventListener(
+    document,
+    "scroll",
+    () => {
+      const shouldShowTheButton = window.scrollY > 1600;
+      setDisplayButton(shouldShowTheButton);
+    },
+    []
+  );
+
   return (
-    <button type="button" className={s.button}>
+    <button type="button" className={`${s.button} ${activeClass}`}>
       <SvgIcon name="right-arrow" />
     </button>
   );
