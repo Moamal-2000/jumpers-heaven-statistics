f33cb3b6cb1eb22623c4ff6dcf33223f3831365b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-04 04:36:46 +0300

    init: FooterNav component

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import SvgIcon from "../Shared/SvgIcon";
 import s from "./Footer.module.scss";
+import FooterNav from "./FooterNav/FooterNav";
 
 const Footer = () => {
   return (
@@ -29,28 +30,7 @@ const Footer = () => {
           </div>
 
           <div className={s.rightSide}>
-            <nav className={s.navigation}>
-              <h3>Navigation</h3>
-
-              <div className={s.links}>
-                <Link href="/">
-                  <SvgIcon name="home" />
-                  <span>Home</span>
-                </Link>
-                <Link href="/maps">
-                  <SvgIcon name="home" />
-                  <span>Maps</span>
-                </Link>
-                <Link href="/players">
-                  <SvgIcon name="home" />
-                  <span>Players</span>
-                </Link>
-                <Link href="/about">
-                  <SvgIcon name="home" />
-                  <span>About</span>
-                </Link>
-              </div>
-            </nav>
+            <FooterNav />
           </div>
         </section>
       </div>
---
+++ frontend/src/Components/Footer/FooterNav/FooterNav.jsx
@@ -0,0 +1,9 @@
+import s from './FooterNav.module.scss'
+
+const FooterNav = () => {
+  return (
+    <div>FooterNav</div>
+  )
+}
+
+export default FooterNav
