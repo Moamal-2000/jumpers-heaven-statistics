37ac894cbfbd354573988c3555a4aa9af66c99ee
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-04 04:38:27 +0300

    feat: add navigation links and styles to FooterNav component

--- frontend/src/Components/Footer/FooterNav/FooterNav.jsx
+++ frontend/src/Components/Footer/FooterNav/FooterNav.jsx
@@ -1,9 +1,32 @@
-import s from './FooterNav.module.scss'
+import SvgIcon from "@/Components/Shared/SvgIcon";
+import Link from "next/link";
+import s from "./FooterNav.module.scss";
 
 const FooterNav = () => {
   return (
-    <div>FooterNav</div>
-  )
-}
+    <nav className={s.navigation}>
+      <h3>Navigation</h3>
 
-export default FooterNav
+      <div className={s.links}>
+        <Link href="/">
+          <SvgIcon name="home" />
+          <span>Home</span>
+        </Link>
+        <Link href="/maps">
+          <SvgIcon name="home" />
+          <span>Maps</span>
+        </Link>
+        <Link href="/players">
+          <SvgIcon name="home" />
+          <span>Players</span>
+        </Link>
+        <Link href="/about">
+          <SvgIcon name="home" />
+          <span>About</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default FooterNav;
