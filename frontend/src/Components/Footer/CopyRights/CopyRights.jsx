625b5c324a62c0a1ec94350bd47b6899bdd3648e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-07 13:32:22 +0300

    update: display the current year dynamically in CopyRights component

--- frontend/src/Components/Footer/CopyRights/CopyRights.jsx
+++ frontend/src/Components/Footer/CopyRights/CopyRights.jsx
@@ -4,9 +4,11 @@ import s from "./CopyRights.module.scss";
 const CopyRights = () => {
   return (
     <p className={s.copyRight}>
-      © 2025 <span>{WEBSITE_NAME}</span>. All rights reserved.
+      © {currentYear} <span>{WEBSITE_NAME}</span>. All rights reserved.
     </p>
   );
 };
 
 export default CopyRights;
+
+const currentYear = new Date().getFullYear();
