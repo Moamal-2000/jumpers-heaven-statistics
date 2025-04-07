1514e7928b8831210f84775cd52aa8fbd813abe1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-07 12:24:58 +0300

    add: html structure & styles to FooterStats component

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import SvgIcon from "../Shared/SvgIcon";
 import s from "./Footer.module.scss";
 import FooterMenusButtons from "./FooterMenusButtons/FooterMenusButtons";
 import FooterNav from "./FooterNav/FooterNav";
+import FooterStats from "./FooterStats/FooterStats";
 
 const Footer = () => {
   return (
@@ -32,36 +33,7 @@ const Footer = () => {
         </section>
 
         <hr className={s.sectionsLine} />
-
-        <section className={s.statistics}>
-          <div className={s.dateWrapper}>
-            <div className={s.iconHolder}>
-              <SvgIcon name="timer" />
-            </div>
-
-            <div className={s.info}>
-              <p className={s.title}>Leaderboard last updated:</p>
-              <p className={s.date}>April 2, 2025 at 08:45 GMT</p>
-            </div>
-          </div>
-
-          <div className={s.statsWrapper}>
-            <div className={s.stat}>
-              <b className={s.number}>42,761</b>
-              <span className={s.title}>Players</span>
-            </div>
-
-            <div className={s.stat}>
-              <b className={s.number}>359</b>
-              <span className={s.title}>Maps</span>
-            </div>
-
-            <div className={s.stat}>
-              <b className={s.number}>183,409</b>
-              <span className={s.title}>Records</span>
-            </div>
-          </div>
-        </section>
+        <FooterStats />
       </div>
     </footer>
   );
--- frontend/src/Components/Footer/FooterStats/FooterStats.jsx
+++ frontend/src/Components/Footer/FooterStats/FooterStats.jsx
@@ -1,9 +1,38 @@
-import s from './FooterStats.module.scss'
+import SvgIcon from "@/Components/Shared/SvgIcon";
+import s from "./FooterStats.module.scss";
 
 const FooterStats = () => {
   return (
-    <section>FooterStats</section>
-  )
-}
+    <section className={s.statsSection}>
+      <div className={s.dateWrapper}>
+        <div className={s.iconHolder}>
+          <SvgIcon name="timer" />
+        </div>
 
-export default FooterStats
+        <div className={s.info}>
+          <p className={s.title}>Leaderboard last updated:</p>
+          <p className={s.date}>April 2, 2025 at 08:45 GMT</p>
+        </div>
+      </div>
+
+      <div className={s.statsWrapper}>
+        <div className={s.stat}>
+          <b className={s.number}>42,761</b>
+          <span className={s.title}>Players</span>
+        </div>
+
+        <div className={s.stat}>
+          <b className={s.number}>359</b>
+          <span className={s.title}>Maps</span>
+        </div>
+
+        <div className={s.stat}>
+          <b className={s.number}>183,409</b>
+          <span className={s.title}>Records</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FooterStats;
