119f8f8282a064a6e23bcbd7cfd77376293fc95c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-07 13:39:14 +0300

    update: reintroduce Introduction component for Players page instead of Home page

--- frontend/src/app/(pages)/players/page.js
+++ frontend/src/app/(pages)/players/page.js
@@ -1,9 +1,11 @@
+import Introduction from "@/Components/Pages/Players/Introduction/Introduction";
 import Players from "@/Components/Pages/Players/Players";
 
 const PlayersPage = () => {
   return (
     <div className="container">
       <main>
+        <Introduction />
         <Players />
       </main>
     </div>
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,4 @@
 import FiltersSection from "@/Components/Pages/Home/FiltersSection/FiltersSection";
-import Introduction from "@/Components/Pages/Home/Introduction/Introduction";
 import LeaderBoard from "@/Components/Pages/Home/LeaderBoard/LeaderBoard";
 import { Suspense } from "react";
 import s from "./page.module.scss";
@@ -8,10 +7,9 @@ export default async function Home() {
   return (
     <div className="container">
       <main className={s.home}>
-        <Introduction />
         <Suspense>
           <FiltersSection />
-          {/* <LeaderBoard /> */}
+          <LeaderBoard />
         </Suspense>
       </main>
     </div>
