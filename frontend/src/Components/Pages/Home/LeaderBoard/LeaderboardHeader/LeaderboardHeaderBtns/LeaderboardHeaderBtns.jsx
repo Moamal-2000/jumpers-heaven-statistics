566c1755ed72fe0f9b630992e8b47cc24f5d8f09
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-12 21:59:14 +0300

    add: the required logic to ShowAllButton component

--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeaderBtns/LeaderboardHeaderBtns.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeaderBtns/LeaderboardHeaderBtns.jsx
@@ -1,91 +1,14 @@
 "use client";
 
-import { PAGINATION_ITEMS_PER_PAGE } from "@/Data/constants";
-import { updateGlobalState } from "@/Redux/slices/globalSlice";
-import { updateLeaderboardState } from "@/Redux/slices/leaderboardSlice";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import ExpandButton from "./ExpandButton";
 import s from "./LeaderboardHeaderBtns.module.scss";
 import ShowAllButton from "./ShowAllButton";
 
 const LeaderboardHeaderBtns = ({ setPaginationNumber }) => {
-  const {
-    leaderboardData,
-    leaderboardScroll,
-    firstChunkLeaderboard,
-    allDataDisplayed,
-    loading,
-    error,
-  } = useSelector((s) => s.leaderboard);
-  const { isLeaderboardReversed } = useSelector((s) => s.global);
-  const dispatch = useDispatch();
-  const isLeaderboardUnavailable =
-    loading || error || leaderboardData.length === 0;
-  const showAllBtnNoun =
-    leaderboardData.length === 0
-      ? "Show All"
-      : allDataDisplayed
-      ? "Show Less"
-      : "Show All";
-
-  function handleShowAllBtn() {
-    if (allDataDisplayed) {
-      handleShowLess();
-      return;
-    }
-
-    handleShowAll();
-  }
-
-  function handleShowAll() {
-    if (leaderboardData?.length <= 0 || allDataDisplayed) return;
-
-    const lastLeaderboardPagination = Math.ceil(
-      leaderboardData?.length / PAGINATION_ITEMS_PER_PAGE
-    );
-
-    dispatch(
-      updateLeaderboardState({
-        key: "leaderboardScroll",
-        value: leaderboardData,
-      })
-    );
-
-    setPaginationNumber(lastLeaderboardPagination);
-  }
-
-  function handleShowLess() {
-    if (leaderboardData?.length <= 0) return;
-
-    dispatch(
-      updateLeaderboardState({
-        key: "leaderboardScroll",
-        value: firstChunkLeaderboard,
-      })
-    );
-    dispatch(updateGlobalState({ key: "isLeaderboardReversed", value: false }));
-
-    setPaginationNumber(1);
-  }
-
-  useEffect(() => {
-    const isSameArrayReference = leaderboardScroll === leaderboardData;
-    if (!isSameArrayReference) handleShowAll();
-  }, [isLeaderboardReversed]);
-
   return (
     <div className={s.buttons}>
       <ExpandButton />
-      <ShowAllButton />
-      <button
-        type="button"
-        className={s.showAllBtn}
-        onClick={handleShowAllBtn}
-        disabled={isLeaderboardUnavailable}
-      >
-        {showAllBtnNoun}
-      </button>
+      <ShowAllButton setPaginationNumber={setPaginationNumber} />
     </div>
   );
 };
--- frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeaderBtns/ShowAllButton.jsx
+++ frontend/src/Components/Pages/Home/LeaderBoard/LeaderboardHeader/LeaderboardHeaderBtns/ShowAllButton.jsx
@@ -1,9 +1,85 @@
-"use client"
+"use client";
 
-const ShowAllButton = () => {
+import { PAGINATION_ITEMS_PER_PAGE } from "@/Data/constants";
+import { updateGlobalState } from "@/Redux/slices/globalSlice";
+import { updateLeaderboardState } from "@/Redux/slices/leaderboardSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const ShowAllButton = ({ setPaginationNumber }) => {
+  const {
+    leaderboardData,
+    leaderboardScroll,
+    firstChunkLeaderboard,
+    allDataDisplayed,
+    loading,
+    error,
+  } = useSelector((s) => s.leaderboard);
+  const { isLeaderboardReversed } = useSelector((s) => s.global);
+  const dispatch = useDispatch();
+  const isLeaderboardUnavailable =
+    loading || error || leaderboardData.length === 0;
+  const showAllBtnNoun =
+    leaderboardData.length === 0
+      ? "Show All"
+      : allDataDisplayed
+      ? "Show Less"
+      : "Show All";
+
+  function handleShowAllBtn() {
+    if (allDataDisplayed) {
+      handleShowLess();
+      return;
+    }
+
+    handleShowAll();
+  }
+
+  function handleShowAll() {
+    if (leaderboardData?.length <= 0 || allDataDisplayed) return;
+
+    const lastLeaderboardPagination = Math.ceil(
+      leaderboardData?.length / PAGINATION_ITEMS_PER_PAGE
+    );
+
+    dispatch(
+      updateLeaderboardState({
+        key: "leaderboardScroll",
+        value: leaderboardData,
+      })
+    );
+
+    setPaginationNumber(lastLeaderboardPagination);
+  }
+
+  function handleShowLess() {
+    if (leaderboardData?.length <= 0) return;
+
+    dispatch(
+      updateLeaderboardState({
+        key: "leaderboardScroll",
+        value: firstChunkLeaderboard,
+      })
+    );
+    dispatch(updateGlobalState({ key: "isLeaderboardReversed", value: false }));
+
+    setPaginationNumber(1);
+  }
+
+  useEffect(() => {
+    const isSameArrayReference = leaderboardScroll === leaderboardData;
+    if (!isSameArrayReference) handleShowAll();
+  }, [isLeaderboardReversed]);
+
   return (
-    <div>ShowAllButton</div>
-  )
-}
+    <button
+      type="button"
+      onClick={handleShowAllBtn}
+      disabled={isLeaderboardUnavailable}
+    >
+      {showAllBtnNoun}
+    </button>
+  );
+};
 
-export default ShowAllButton
+export default ShowAllButton;
