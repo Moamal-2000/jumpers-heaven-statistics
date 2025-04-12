7260381f0e33c55fbd48281dfd900cd5ec81e3d9
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-12 22:20:31 +0300

    feat: add scroll-to-top/down behavior based on scroll position in ScrollToTopBtn component

--- frontend/src/Components/Shared/Buttons/ScrollToTopBtn/ScrollToTopBtn.jsx
+++ frontend/src/Components/Shared/Buttons/ScrollToTopBtn/ScrollToTopBtn.jsx
@@ -5,28 +5,37 @@ import SvgIcon from "../../SvgIcon";
 import s from "./ScrollToTopBtn.module.scss";
 
 const ScrollToTopBtn = () => {
-  const [displayButton, setDisplayButton] = useState(false);
-  const activeClass = displayButton ? s.active : "";
+  const [isVisible, setIsVisible] = useState(false);
+  const [isAtTopHalf, setIsAtTopHalf] = useState(true);
+
+  const buttonClass = `${s.button} ${isVisible ? s.active : ""} ${
+    isAtTopHalf ? s.reverse : ""
+  }`;
+
+  const scrollToPosition = () => {
+    const pageHeight = document.documentElement.scrollHeight;
+    const isAtTop = window.scrollY < pageHeight / 2;
+
+    window.scrollTo({
+      top: isAtTop ? pageHeight : 0,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
     const handleScroll = () => {
-      setDisplayButton(window.scrollY > 1600);
+      const pageHeight = document.documentElement.scrollHeight;
+
+      setIsAtTopHalf(window.scrollY < pageHeight / 2);
+      setIsVisible(window.scrollY > 1600);
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  function handleClick() {
-    window?.scrollTo({ top: 0, behavior: "smooth" });
-  }
-
   return (
-    <button
-      type="button"
-      className={`${s.button} ${activeClass}`}
-      onClick={handleClick}
-    >
+    <button type="button" className={buttonClass} onClick={scrollToPosition}>
       <SvgIcon name="right-arrow" />
     </button>
   );
