07e49dcb363ff935281a17b567eea25688def2fd
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-12 13:41:33 +0300

    init: Introduction component for Maps page

---
+++ frontend/src/Components/Pages/Maps/Introduction/Introduction.jsx
@@ -0,0 +1,9 @@
+import s from './Introduction.module.scss'
+
+const Introduction = () => {
+  return (
+    <div>Introduction</div>
+  )
+}
+
+export default Introduction
--- frontend/src/app/(pages)/maps/page.js
+++ frontend/src/app/(pages)/maps/page.js
@@ -1,11 +1,13 @@
 import Maps from "@/Components/Pages/Maps/Maps";
 import s from "./MapsPage.module.scss";
+import Introduction from "@/Components/Pages/Maps/Introduction/Introduction";
 
 const MapsPage = () => {
   return (
     <div className="container">
       <main className={s.mapsPage}>
-        <Maps />
+        <Introduction />
+        {/* <Maps /> */}
       </main>
     </div>
   );
