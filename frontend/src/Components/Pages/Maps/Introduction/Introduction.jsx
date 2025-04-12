825bfa5595c494dce4573081ace5c76815f6a244
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-12 13:44:05 +0300

    feat: add html structure/ css selectors to Introduction component

--- frontend/src/Components/Pages/Maps/Introduction/Introduction.jsx
+++ frontend/src/Components/Pages/Maps/Introduction/Introduction.jsx
@@ -1,9 +1,19 @@
-import s from './Introduction.module.scss'
+import s from "./Introduction.module.scss";
 
 const Introduction = () => {
   return (
-    <div>Introduction</div>
-  )
-}
+    <section className={s.introduction}>
+      <h2>
+        Discover Amazing <span>Maps</span>
+      </h2>
 
-export default Introduction
+      <p>
+        Explore a vast collection of player-created challenges ranging from
+        beginner-friendly courses to expert-level speedruns. Find your next
+        favorite jump challenge and become a legend.
+      </p>
+    </section>
+  );
+};
+
+export default Introduction;
