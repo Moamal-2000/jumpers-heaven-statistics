714f298041471453049670a8e0646b09753eee66
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-13 18:13:40 +0300

    init: leaderboards route

---
+++ frontend/src/app/(pages)/leaderboards/page.js
@@ -0,0 +1,12 @@
+import s from "./LeaderboardsPage.module.scss";
+
+const LeaderboardsPage = () => {
+  return (
+    <div className="container">
+      <main className={s.leaderboardsPage}>
+      </main>
+    </div>
+  );
+};
+
+export default LeaderboardsPage;
