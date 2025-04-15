ab18ba4e27d445d18c0bba71aea1fb7c69fcacb3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-15 17:14:52 +0300

    write: html structure & css selector to MobileNavMenu component

--- frontend/src/Components/Header/MobileNavMenu/MobileNavMenu.jsx
+++ frontend/src/Components/Header/MobileNavMenu/MobileNavMenu.jsx
@@ -1,9 +1,25 @@
-import s from './MobileNavMenu.module.scss'
+import SvgIcon from "@/Components/Shared/SvgIcon";
+import { NAV_LINKS_DATA } from "@/Data/staticData";
+import Link from "next/link";
+import s from "./MobileNavMenu.module.scss";
 
 const MobileNavMenu = () => {
   return (
-    <div>MobileNavMenu</div>
-  )
-}
+    <nav className={s.mobileNav}>
+      <p className={s.title}>Main Navigation</p>
 
-export default MobileNavMenu
+      <ul className={s.links}>
+        {NAV_LINKS_DATA.map(({ name, href, iconName, id }) => (
+          <li key={id}>
+            <Link href={href} className={s.link}>
+              <SvgIcon name={iconName} />
+              <span>{name}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default MobileNavMenu;
