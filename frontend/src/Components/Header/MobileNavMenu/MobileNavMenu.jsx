c76c3c504fc5165d7f9648fc90f17ee91dac3be3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-15 17:28:37 +0300

    feat: highlight active link based on current page in MobileNavMenu

--- frontend/src/Components/Header/MobileNavMenu/MobileNavMenu.jsx
+++ frontend/src/Components/Header/MobileNavMenu/MobileNavMenu.jsx
@@ -1,22 +1,31 @@
+"use client";
+
 import SvgIcon from "@/Components/Shared/SvgIcon";
 import { NAV_LINKS_DATA } from "@/Data/staticData";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import s from "./MobileNavMenu.module.scss";
 
 const MobileNavMenu = () => {
+  const currentPage = usePathname();
+
   return (
     <nav className={s.mobileNav}>
       <p className={s.title}>Main Navigation</p>
 
       <ul className={s.links}>
-        {NAV_LINKS_DATA.map(({ name, href, iconName, id }) => (
-          <li key={id}>
-            <Link href={href} className={`${s.link}`}>
-              <SvgIcon name={iconName} />
-              <span>{name}</span>
-            </Link>
-          </li>
-        ))}
+        {NAV_LINKS_DATA.map(({ name, href, iconName, id }) => {
+          const activeClass = currentPage === href ? s.active : "";
+
+          return (
+            <li key={id}>
+              <Link href={href} className={`${s.link} ${activeClass}`}>
+                <SvgIcon name={iconName} />
+                <span>{name}</span>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
