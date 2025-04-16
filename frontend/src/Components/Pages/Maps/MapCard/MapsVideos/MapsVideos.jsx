2d62c4d40f32f7f374ccfe1e0b0247dd402af1e6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-17 00:49:40 +0300

    feat: add click handler to video buttons to open video links in a new tab

--- frontend/src/Components/Pages/Maps/MapCard/MapsVideos/MapsVideos.jsx
+++ frontend/src/Components/Pages/Maps/MapCard/MapsVideos/MapsVideos.jsx
@@ -2,12 +2,16 @@ import SvgIcon from "@/Components/Shared/SvgIcon";
 import s from "./MapsVideos.module.scss";
 
 const MapsVideos = ({ videos }) => {
+  function visitVideo() {
+    window.open(videos[0].videoUrl, "_blank");
+  }
+
   return (
     <div className={s.videos}>
       {!videos?.length && <p>This map has no videos.</p>}
 
       {videos?.map(({ type, icon, id }) => (
-        <button type="button" className={s.video} key={id}>
+        <button type="button" className={s.video} key={id} onClick={visitVideo}>
           <SvgIcon name={icon} />
           <span className={s.type}>{type}</span>
         </button>
