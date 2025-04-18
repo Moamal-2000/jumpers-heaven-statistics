e85c4d3ea6100f63f01b1233236a424752d68abf
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-18 19:53:15 +0300

    init: FiltersSection component for Maps page

---
+++ frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
@@ -0,0 +1,7 @@
+import s from "./FiltersSection.module.scss";
+
+const FiltersSection = () => {
+  return <section className={s.filtersSection}></section>;
+};
+
+export default FiltersSection;
--- frontend/src/app/(pages)/maps/page.js
+++ frontend/src/app/(pages)/maps/page.js
@@ -1,12 +1,14 @@
+import FiltersSection from "@/Components/Pages/Maps/FiltersSection/FiltersSection";
+import Introduction from "@/Components/Pages/Maps/Introduction/Introduction";
 import Maps from "@/Components/Pages/Maps/Maps";
 import s from "./MapsPage.module.scss";
-import Introduction from "@/Components/Pages/Maps/Introduction/Introduction";
 
 const MapsPage = () => {
   return (
     <div className="container">
       <main className={s.mapsPage}>
         <Introduction />
+        <FiltersSection />
         <Maps />
       </main>
     </div>
