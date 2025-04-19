3e9fe0a25672613d3442496539cb2b6d3d1e50cd
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-19 20:23:59 +0300

    feat: add tooltip to legend element in FilterGroup component

--- frontend/src/Components/Pages/Maps/FiltersSection/FilterGroup/FilterGroup.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/FilterGroup/FilterGroup.jsx
@@ -1,9 +1,19 @@
+import SvgIcon from "@/Components/Shared/SvgIcon";
 import s from "./FilterGroup.module.scss";
 
 const FilterGroup = () => {
   return (
     <fieldset className={s.filterGroup}>
-      <legend className={s.label}>Map Type</legend>
+      <legend className={s.label}>
+        Map Type
+        <div className={s.tooltip}>
+          <div className={s.icon}>
+            <SvgIcon name="questionMark" />
+          </div>
+
+          <p className={s.tooltipText}>Filter maps by their gameplay type</p>
+        </div>
+      </legend>
 
       <div className={s.filterButtons}>
         <button type="button">Speedrun</button>
--- frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
@@ -1,7 +1,10 @@
+import FilterGroup from "./FilterGroup/FilterGroup";
 import s from "./FiltersSection.module.scss";
 
 const FiltersSection = () => {
-  return <section className={s.filtersSection}></section>;
+  return <section className={s.filtersSection}>
+    <FilterGroup />
+  </section>;
 };
 
 export default FiltersSection;
