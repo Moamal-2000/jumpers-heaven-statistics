c2d04a9a4d89c55cc6a6a0aef50f26613a79eab8
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-20 00:06:32 +0300

    fix: wrap FilterGroup in Suspense (deploying issue)

--- frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
@@ -1,4 +1,5 @@
 import { MAPS_FILTERS_DATA } from "@/Data/filters";
+import { Suspense } from "react";
 import FilterGroup from "./FilterGroup/FilterGroup";
 import s from "./FiltersSection.module.scss";
 
@@ -7,16 +8,18 @@ const FiltersSection = () => {
     <section className={s.filtersSection}>
       {MAPS_FILTERS_DATA.map(
         ({ label, queryName, defaultUrlQuery, filtersData, tooltipText }) => (
-          <FilterGroup
-            key={queryName}
-            {...{
-              label,
-              queryName,
-              defaultUrlQuery,
-              filtersData,
-              tooltipText,
-            }}
-          />
+          <Suspense>
+            <FilterGroup
+              key={queryName}
+              {...{
+                label,
+                queryName,
+                defaultUrlQuery,
+                filtersData,
+                tooltipText,
+              }}
+            />
+          </Suspense>
         )
       )}
     </section>
