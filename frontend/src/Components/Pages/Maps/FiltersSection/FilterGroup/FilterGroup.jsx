9264c733ef674bfafcca77e2a1ad5305987fda50
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-19 21:07:57 +0300

    update: FilterGroup component to accept dynamic props & display Maps filters

--- frontend/src/Components/Pages/Maps/FiltersSection/FilterGroup/FilterGroup.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/FilterGroup/FilterGroup.jsx
@@ -1,25 +1,32 @@
 import SvgIcon from "@/Components/Shared/SvgIcon";
 import s from "./FilterGroup.module.scss";
 
-const FilterGroup = () => {
+const FilterGroup = ({
+  label,
+  queryName,
+  defaultUrlQuery,
+  filtersData,
+  tooltipText,
+}) => {
   return (
     <fieldset className={s.filterGroup}>
       <legend className={s.label}>
-        Map Type
+        {label}
         <div className={s.tooltip}>
           <div className={s.icon}>
             <SvgIcon name="questionMark" />
           </div>
 
-          <p className={s.tooltipText}>Filter maps by their gameplay type</p>
+          <p className={s.tooltipText}>{tooltipText}</p>
         </div>
       </legend>
 
       <div className={s.filterButtons}>
-        <button type="button">Speedrun</button>
-        <button type="button">Skilled</button>
-        <button type="button">Defrag</button>
-        <button type="button">Surf</button>
+        {filtersData?.map(({ text, queryValue, id }) => (
+          <button type="button" key={id}>
+            {text}
+          </button>
+        ))}
       </div>
     </fieldset>
   );
--- frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
@@ -1,10 +1,18 @@
+import { MAPS_FILTERS_DATA } from "@/Data/filters";
 import FilterGroup from "./FilterGroup/FilterGroup";
 import s from "./FiltersSection.module.scss";
 
 const FiltersSection = () => {
   return (
     <section className={s.filtersSection}>
-      <FilterGroup />
+      {MAPS_FILTERS_DATA.map(
+        ({ label, queryName, defaultUrlQuery, filtersData, tooltipText }) => (
+          <FilterGroup
+            key={queryName}
+            {...{ label, queryName, defaultUrlQuery, filtersData, tooltipText }}
+          />
+        )
+      )}
     </section>
   );
 };
