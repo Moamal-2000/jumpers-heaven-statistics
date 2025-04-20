c01f5b086bb7127d6fc18dba2b0951a028e5ec71
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-20 14:55:44 +0300

    update: wrap FiltersSection in Suspense instead of wrapping it at FilterGroup component

--- frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
+++ frontend/src/Components/Pages/Maps/FiltersSection/FiltersSection.jsx
@@ -1,5 +1,4 @@
 import { MAPS_FILTERS_DATA } from "@/Data/filters";
-import { Suspense } from "react";
 import FilterGroup from "./FilterGroup/FilterGroup";
 import s from "./FiltersSection.module.scss";
 
@@ -8,18 +7,16 @@ const FiltersSection = () => {
     <section className={s.filtersSection}>
       {MAPS_FILTERS_DATA.map(
         ({ label, queryName, defaultUrlQuery, filtersData, tooltipText }) => (
-          <Suspense>
-            <FilterGroup
-              key={queryName}
-              {...{
-                label,
-                queryName,
-                defaultUrlQuery,
-                filtersData,
-                tooltipText,
-              }}
-            />
-          </Suspense>
+          <FilterGroup
+            key={queryName}
+            {...{
+              label,
+              queryName,
+              defaultUrlQuery,
+              filtersData,
+              tooltipText,
+            }}
+          />
         )
       )}
     </section>
--- frontend/src/app/(pages)/maps/page.js
+++ frontend/src/app/(pages)/maps/page.js
@@ -1,6 +1,7 @@
 import FiltersSection from "@/Components/Pages/Maps/FiltersSection/FiltersSection";
 import Introduction from "@/Components/Pages/Maps/Introduction/Introduction";
 import Maps from "@/Components/Pages/Maps/Maps";
+import { Suspense } from "react";
 import s from "./MapsPage.module.scss";
 
 const MapsPage = () => {
@@ -8,7 +9,9 @@ const MapsPage = () => {
     <div className="container">
       <main className={s.mapsPage}>
         <Introduction />
-        <FiltersSection />
+        <Suspense>
+          <FiltersSection />
+        </Suspense>
         <Maps />
       </main>
     </div>
