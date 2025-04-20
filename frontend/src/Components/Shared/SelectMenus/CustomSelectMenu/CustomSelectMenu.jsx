1851fac985328e776d62fe1d361c01de50a0f583
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-20 22:12:48 +0300

    fix: CustomSelectMenu to handle toggle/open/close logic without useEventListener to avoid SSR 'document' error

--- frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu.jsx
+++ frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu.jsx
@@ -1,29 +1,40 @@
 "use client";
 
 import { SORT_MAPS_OPTIONS } from "@/Data/staticData";
-import useEventListener from "@/Hooks/Helper/useEventListener";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SvgIcon from "../../SvgIcon";
 import s from "./CustomSelectMenu.module.scss";
 
 const CustomSelectMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
   const visibleClass = isOpen ? `${s.visible}` : "";
 
   function handleClick() {
-    setIsOpen(true);
+    setIsOpen((prev) => !prev);
   }
 
-  useEventListener(document, "click", () => setIsOpen(false));
+  function handleClickOutside(event) {
+    const isSelectMenu = menuRef.current.contains(event.target);
+    const isOption = event.target.closest(`.${s.optionsList}`);
+    const shouldCloseMenu = (menuRef.current && !isSelectMenu) || isOption;
+
+    if (shouldCloseMenu) setIsOpen(false);
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
 
   return (
-    <div className={`${s.selectMenu} ${visibleClass}`}>
+    <div ref={menuRef} className={`${s.selectMenu} ${visibleClass}`}>
       <button type="button" className={s.selectButton} onClick={handleClick}>
         <span>Newest First</span>
         <SvgIcon name="solidArrow" />
       </button>
 
-      <ul className={s.optionsList}>
+      <ul className={s.optionsList} data-type="sort-maps-options">
         {SORT_MAPS_OPTIONS.map(({ label, value, id }) => (
           <li key={id}>{label}</li>
         ))}
