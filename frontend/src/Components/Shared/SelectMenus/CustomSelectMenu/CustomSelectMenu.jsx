5453d6ef8be862d45dddaed70e116db733e31fec
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-20 21:40:31 +0300

    feat: active drop-down menu by clicking on CustomSelectMenu button

--- frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu.jsx
+++ frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu.jsx
@@ -1,18 +1,32 @@
+"use client";
+
+import { SORT_MAPS_OPTIONS } from "@/Data/staticData";
+import useEventListener from "@/Hooks/Helper/useEventListener";
+import { useState } from "react";
 import SvgIcon from "../../SvgIcon";
 import s from "./CustomSelectMenu.module.scss";
 
 const CustomSelectMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const visibleClass = isOpen ? `${s.visible}` : "";
+
+  function handleClick() {
+    setIsOpen(true);
+  }
+
+  useEventListener(document, "click", () => setIsOpen(false));
+
   return (
-    <div className={s.selectMenu}>
-      <button type="button" className={s.selectButton}>
+    <div className={`${s.selectMenu} ${visibleClass}`}>
+      <button type="button" className={s.selectButton} onClick={handleClick}>
         <span>Newest First</span>
         <SvgIcon name="solidArrow" />
       </button>
 
       <ul className={s.optionsList}>
-        <li>Newest First</li>
-        <li>Oldest First</li>
-        <li>Name (A-Z)</li>
+        {SORT_MAPS_OPTIONS.map(({ label, value, id }) => (
+          <li key={id}>{label}</li>
+        ))}
       </ul>
     </div>
   );
