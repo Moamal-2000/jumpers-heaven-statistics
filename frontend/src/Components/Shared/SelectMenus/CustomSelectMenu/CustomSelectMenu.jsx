f43859bcb5cb3a13781784252fe8e18721d88bfd
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-21 18:48:32 +0300

    refactor: CustomSelectMenu to use DropDownMenu component

--- frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu.jsx
+++ frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/CustomSelectMenu.jsx
@@ -1,47 +1,28 @@
 "use client";
 
-import { SORT_MAPS_OPTIONS } from "@/Data/staticData";
-import {
-  createQueryString,
-  getSortByLabel,
-  removeQueryString,
-} from "@/Functions/utils";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getSortByLabel } from "@/Functions/utils";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import SvgIcon from "../../SvgIcon";
 import s from "./CustomSelectMenu.module.scss";
+import DropDownMenu from "./DropDownMenu/DropDownMenu";
 
 const CustomSelectMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef(null);
   const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
   const urlQuery = searchParams.get("sort-by");
   const [currentSortBy, setCurrentSortBy] = useState(getSortByLabel(urlQuery));
-  const visibleClass = isOpen ? `${s.visible}` : "";
 
   function handleClick() {
     setIsOpen((prev) => !prev);
   }
 
-  function handleSelectOption(value, label) {
-    const isDefault = value === "newest";
-
-    if (isDefault) {
-      setCurrentSortBy("Newest First");
-      removeQueryString("sort-by", searchParams, router, pathname);
-      return;
-    }
-
-    setCurrentSortBy(label);
-    createQueryString("sort-by", value, searchParams, router, pathname);
-  }
-
   useEffect(() => {
     function handleClickOutside(event) {
       const isSelectMenu = menuRef.current.contains(event.target);
-      const isOption = event.target.closest(`.${s.optionsList}`);
+      const elementRole = event.target?.closest("button")?.getAttribute("role");
+      const isOption = elementRole === "option";
       const shouldCloseMenu = (menuRef.current && !isSelectMenu) || isOption;
 
       if (shouldCloseMenu) setIsOpen(false);
@@ -52,35 +33,17 @@ const CustomSelectMenu = () => {
   }, []);
 
   return (
-    <div ref={menuRef} className={`${s.selectMenu} ${visibleClass}`}>
+    <div ref={menuRef} className={s.selectMenu}>
       <button type="button" className={s.selectButton} onClick={handleClick}>
         <span>{currentSortBy}</span>
         <SvgIcon name="solidArrow" />
       </button>
 
-      <div
-        role="listbox"
-        aria-label="Sort maps options"
-        className={s.optionsList}
-        data-type="sort-maps-options"
-      >
-        {SORT_MAPS_OPTIONS.map(({ label, value, id }) => {
-          const isActive = currentSortBy === label;
-
-          return (
-            <button
-              key={id}
-              type="button"
-              role="option"
-              aria-selected={isActive}
-              className={isActive ? s.active : ""}
-              onClick={() => handleSelectOption(value, label)}
-            >
-              {label}
-            </button>
-          );
-        })}
-      </div>
+      <DropDownMenu
+        isOpen={isOpen}
+        currentSortBy={currentSortBy}
+        setCurrentSortBy={setCurrentSortBy}
+      />
     </div>
   );
 };
---
+++ frontend/src/Components/Shared/SelectMenus/CustomSelectMenu/DropDownMenu/DropDownMenu.jsx
@@ -0,0 +1,51 @@
+import { SORT_MAPS_OPTIONS } from "@/Data/staticData";
+import { createQueryString, removeQueryString } from "@/Functions/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import s from "./DropDownMenu.module.scss";
+
+const DropDownMenu = ({ isOpen, currentSortBy, setCurrentSortBy }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const visibleClass = isOpen ? `${s.visible}` : "";
+
+  function handleSelectOption(value, label) {
+    const isDefault = value === "newest";
+
+    if (isDefault) {
+      setCurrentSortBy("Newest First");
+      removeQueryString("sort-by", searchParams, router, pathname);
+      return;
+    }
+
+    setCurrentSortBy(label);
+    createQueryString("sort-by", value, searchParams, router, pathname);
+  }
+
+  return (
+    <div
+      role="listbox"
+      aria-label="Sort maps options"
+      className={`${s.optionsList} ${visibleClass}`}
+    >
+      {SORT_MAPS_OPTIONS.map(({ label, value, id }) => {
+        const isActive = currentSortBy === label;
+
+        return (
+          <button
+            key={id}
+            type="button"
+            role="option"
+            aria-selected={isActive}
+            className={isActive ? s.active : ""}
+            onClick={() => handleSelectOption(value, label)}
+          >
+            {label}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default DropDownMenu;
