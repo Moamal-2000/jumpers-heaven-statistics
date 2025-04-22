7864a5049944c646855023c027e28e64b8cfc5ca
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-22 13:23:03 +0300

    rename: getFilterKeyFromDate to getLastSeenCategories & some variables for clean code

--- frontend/src/Functions/filters.js
+++ frontend/src/Functions/filters.js
@@ -1,25 +1,24 @@
-export function getFilterKeyFromDate(lastSeen) {
+export function getLastSeenCategories(lastSeen) {
   const now = new Date();
   const seenDate = new Date(lastSeen);
-
   const diffInMs = now - seenDate;
   const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
-  const periods = ["today", "this week", "this month", "long time"];
+  const filters = ["today", "this week", "this month", "long time"];
 
-  if (diffInDays < 1) return periods.slice(0, 3);
-  if (diffInDays <= 7) return periods.slice(1, 3);
-  if (diffInDays <= 30) return periods.slice(2, 4);
-  return [periods.at(-1)];
+  if (diffInDays < 1) return filters.slice(0, 3);
+  if (diffInDays <= 7) return filters.slice(1, 3);
+  if (diffInDays <= 30) return filters.slice(2, 4);
+  return [filters.at(-1)];
 }
 
-export function getIsPlayerKey(lastSeen, filterKey) {
-  const playerKey = getFilterKeyFromDate(lastSeen);
-  return playerKey?.includes(filterKey);
+export function matchesFilterKey(lastSeen, filterKey) {
+  const filters = getLastSeenCategories(lastSeen);
+  return filters?.includes(filterKey);
 }
 
 export function getLastSeenLeaderboard(data, filterKey) {
   if (!filterKey) return data;
-  return data?.filter((item) => getIsPlayerKey(item.LastSeen, filterKey));
+  return data?.filter((item) => matchesFilterKey(item.LastSeen, filterKey));
 }
 
 export function getRegionLeaderboard(data, filterKey) {
