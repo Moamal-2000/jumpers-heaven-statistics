78f21a8a12bb3ab8e34de40dbadd5d63e913c98a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-23 17:40:05 +0300

    remove: fetching data & html structure from PlayerId page /player/[id]

--- frontend/src/app/(pages)/player/[playerId]/page.js
+++ frontend/src/app/(pages)/player/[playerId]/page.js
@@ -1,44 +1,20 @@
-import { jhApis } from "@/Api/jumpersHeaven";
-import { getColoredName } from "@/Functions/components";
-import { decodeAsyncData } from "@/Functions/utils";
-
 const PlayerId = async ({ params }) => {
   const { playerId } = await params;
 
-  const playersResponse = await fetch(
-    jhApis({ fps: 125 }).leaderboard.getSpeedRunLeaderboard,
-    { headers: { Accept: "application/msgpack" } }
-  );
+  // const playersResponse = await fetch(
+  //   jhApis({ fps: 125 }).leaderboard.getSpeedRunLeaderboard,
+  //   { headers: { Accept: "application/msgpack" } }
+  // );
 
-  const playersData = await decodeAsyncData(playersResponse);
-  const playerData = playersData.find(
-    ({ PlayerID }) => +PlayerID === +playerId
-  );
-
-  const { PlayerName, Score, TopList } = playerData;
+  // const playersData = await decodeAsyncData(playersResponse);
+  // const playerData = playersData.find(
+  //   ({ PlayerID }) => +PlayerID === +playerId
+  // );
 
   return (
     <main>
       <h2>Testing</h2>
-
-      <br />
-
-      <ul style={{ listStyleType: "none" }}>
-        <li>Player: {getColoredName(PlayerName)}</li>
-        <li>
-          Player ID: <span style={{ color: "yellow" }}>{playerId}</span>
-        </li>
-        <li>
-          Score: <span style={{ color: "orange" }}>{Score}</span>
-        </li>
-        <li>
-          Tops: [
-          <span style={{ color: "cyan" }}>
-            {Object.values(TopList).join(", ")}
-          </span>
-          ]
-        </li>
-      </ul>
+      <p>player id: {playerId}</p>
     </main>
   );
 };
