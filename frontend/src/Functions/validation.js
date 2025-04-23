32ff64e6d4a64870d7d6adcdd020598c6b1763cf
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-23 17:47:56 +0300

    fix: add guard clause in isMobile function to handle if the function executing by server-side

--- frontend/src/Functions/validation.js
+++ frontend/src/Functions/validation.js
@@ -21,6 +21,8 @@ export function shouldRenderPaginationBtn(numberOfPages, index, currentPage) {
 }
 
 export function isMobile() {
+  if (typeof navigator === "undefined") return;
+
   const mobilesType = /Mobi|Android|iPhone|iPad|iPod/i;
-  return mobilesType.test(navigator?.userAgent);
+  return mobilesType.test(navigator.userAgent);
 }
