0aa12562cfb61dbf6b8bc648d7731d50e1e25008
Moamal-2000 <moamalalaapro1@gmail.com>
2025-04-26 18:47:54 +0300

    feat: close menu and overlay when a nav link is clicked for MobileNavMenu

--- frontend/src/Components/Header/MobileNavMenu/MobileNavMenu.jsx
+++ frontend/src/Components/Header/MobileNavMenu/MobileNavMenu.jsx
@@ -3,15 +3,17 @@
 import CopyRights from "@/Components/Footer/CopyRights/CopyRights";
 import SvgIcon from "@/Components/Shared/SvgIcon";
 import { NAV_LINKS_DATA } from "@/Data/staticData";
+import { updateGlobalState } from "@/Redux/slices/globalSlice";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import s from "./MobileNavMenu.module.scss";
 
 const MobileNavMenu = () => {
   const currentPage = usePathname();
   const { isMobileNavActive } = useSelector((s) => s.global);
+  const dispatch = useDispatch();
   const activeClass = isMobileNavActive ? s.active : "";
 
   useEffect(() => {
@@ -24,11 +26,28 @@ const MobileNavMenu = () => {
 
       <ul className={s.links}>
         {NAV_LINKS_DATA.map(({ name, href, iconName, id }) => {
-          const activeClass = currentPage === href ? s.active : "";
+          const isCurrentPage = currentPage === href;
+          const activeClass = isCurrentPage ? s.active : "";
+
+          function handleLinkClick() {
+            if (isCurrentPage) return;
+
+            dispatch(
+              updateGlobalState({ key: "isMobileNavActive", value: false })
+            );
+
+            dispatch(
+              updateGlobalState({ key: "isGlobalOverlayActive", value: false })
+            );
+          }
 
           return (
             <li key={id}>
-              <Link href={href} className={`${s.link} ${activeClass}`}>
+              <Link
+                href={href}
+                className={`${s.link} ${activeClass}`}
+                onClick={handleLinkClick}
+              >
                 <SvgIcon name={iconName} />
                 <span>{name}</span>
               </Link>
