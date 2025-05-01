901c7249dceacfcb3cb8407bcb0e2a93f09b38bc
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-01 16:54:39 +0300

    feat: use local SVG stars icons for MapStars component

--- frontend/src/Components/Pages/Maps/MapCard2/MapStars/MapStars.jsx
+++ frontend/src/Components/Pages/Maps/MapCard2/MapStars/MapStars.jsx
@@ -1,21 +1,21 @@
+import SvgIcon from "@/Components/Shared/SvgIcon";
 import { NUMBER_OF_RATING_STARS } from "@/Data/constants";
 import s from "./MapStars.module.scss";
 
 const MapStars = ({ rate }) => {
-  return (
-    <div className={s.stars}>
-      {Array.from({ length: NUMBER_OF_RATING_STARS }, (_, i) => (
-        <span key={i} className={getStarClass(i, rate)} />
-      ))}
-    </div>
+  const starsIcons = Array.from(
+    { length: NUMBER_OF_RATING_STARS },
+    (_, index) => <SvgIcon key={index} name={getStarName(index, rate)} />
   );
+
+  return <div className={s.stars}>{starsIcons}</div>;
 };
 
 export default MapStars;
 
-function getStarClass(index, rate) {
+function getStarName(index, rate = 3.5) {
   const starValue = index + 1;
-  if (rate >= starValue) return s.star;
-  if (rate >= starValue - 0.5) return `${s.star} ${s.half}`;
-  return `${s.star} ${s.empty}`;
+  if (rate >= starValue) return "yellowStar";
+  if (rate >= starValue - 0.5) return "halfStar";
+  return "emptyStar";
 }
--- frontend/src/Data/iconsData.jsx
+++ frontend/src/Data/iconsData.jsx
@@ -1,5 +1,38 @@
 export const iconsData = [
   {
+    name: "yellowStar",
+    icon: (
+      <svg fill="#ffc107" viewBox="0 0 24 24">
+        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+      </svg>
+    ),
+  },
+  {
+    name: "halfStar",
+    icon: (
+      <svg viewBox="0 0 24 24">
+        <defs>
+          <linearGradient id="a" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="50%" stop-color="#ffc107" />
+            <stop offset="50%" stop-color="#475569" />
+          </linearGradient>
+        </defs>
+        <path
+          fill="url(#a)"
+          d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "emptyStar",
+    icon: (
+      <svg fill="#475569" viewBox="0 0 24 24">
+        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+      </svg>
+    ),
+  },
+  {
     name: "trophy",
     icon: (
       <svg
