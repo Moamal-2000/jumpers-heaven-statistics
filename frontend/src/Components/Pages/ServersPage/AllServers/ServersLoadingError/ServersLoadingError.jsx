13b8eba329804adc477b5f2218d07b9604e2f066
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-20 16:52:54 +0300

    init: ServersLoadingError component

---
+++ frontend/src/Components/Pages/ServersPage/AllServers/ServersLoadingError/ServersLoadingError.jsx
@@ -0,0 +1,9 @@
+import s from './ServersLoadingError.module.scss'
+
+const ServersLoadingError = () => {
+  return (
+    <div>ServersLoadingError</div>
+  )
+}
+
+export default ServersLoadingError
